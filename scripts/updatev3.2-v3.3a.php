ddaefd5339d0d299958d68faf8d20b84e954a010
wim <wim@b1110c55-7f20-0410-b8ce-a3631e077a91>
2008-09-14 13:36:29 +0000

    ASSIGNED - bug 3: Add extra fields when adding a new observation to make DeepskyLog compatible with the ComAST XML scheme http://www.deepskylog.org/bugs/show_bug.cgi?id=3
    Added missing variables to database.

---
+++ scripts/updatev3.2-v3.3a.php
@@ -0,0 +1,47 @@
+<?php
+include_once "../lib/database.php";
+
+$db = new database;
+$db->login();
+
+// Insert the estimated small and large diameter in arc minutes
+$sql="ALTER TABLE observations ADD COLUMN smallDiameter float NOT NULL default ''";
+$run = mysql_query($sql) or die(mysql_error());
+
+$sql="ALTER TABLE observations ADD COLUMN largeDiameter float NOT NULL default ''";
+$run = mysql_query($sql) or die(mysql_error());
+
+// stellar
+$sql="ALTER TABLE observations ADD COLUMN stellar int(1) NOT NULL default ''";
+$run = mysql_query($sql) or die(mysql_error());
+
+// extended
+$sql="ALTER TABLE observations ADD COLUMN extended int(1) NOT NULL default ''";
+$run = mysql_query($sql) or die(mysql_error());
+
+// resolved
+$sql="ALTER TABLE observations ADD COLUMN resolved int(1) NOT NULL default ''";
+$run = mysql_query($sql) or die(mysql_error());
+
+// mottled
+$sql="ALTER TABLE observations ADD COLUMN mottled int(1) NOT NULL default ''";
+$run = mysql_query($sql) or die(mysql_error());
+
+// Only for open clusters : characterType, see page 17 of http://deepsky.fg-vds.de/download/dsl8.pdf
+$sql="ALTER TABLE observations ADD COLUMN characterType varchar(1) NOT NULL default ''";
+$run = mysql_query($sql) or die(mysql_error());
+
+// Only for open clusters : unusualShape
+$sql="ALTER TABLE observations ADD COLUMN unusualShape int(1) NOT NULL default ''";
+$run = mysql_query($sql) or die(mysql_error());
+
+// Only for open clusters : partlyUnresolved
+$sql="ALTER TABLE observations ADD COLUMN partlyUnresolved int(1) NOT NULL default ''";
+$run = mysql_query($sql) or die(mysql_error());
+
+// Only for open clusters : colorContrasts
+$sql="ALTER TABLE observations ADD COLUMN colorContrasts int(1) NOT NULL default ''";
+$run = mysql_query($sql) or die(mysql_error());
+
+print "Database update was successful!"
+?>
