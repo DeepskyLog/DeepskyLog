eecaf7208ffb6e169f128fe0fcad461e7eea1a74
david <david@b1110c55-7f20-0410-b8ce-a3631e077a91>
2008-09-17 17:37:14 +0000

    Corrections

--- sql/updatev3.2-v3.3a.sql
+++ sql/updatev3.2-v3.3a.sql
@@ -1,17 +1,17 @@
-﻿CREATE TABLE `atlasses` (
+CREATE TABLE `atlasses` (
   `atlasCode` VARCHAR(100) NOT NULL DEFAULT 'Atlas',
   PRIMARY KEY (`atlasCode`)
 );
 
-INSERT INTO atlasses(atlasCode, atlasNr) VALUES ('urano');
-INSERT INTO atlasses(atlasCode, atlasNr) VALUES ('urano_new');
-INSERT INTO atlasses(atlasCode, atlasNr) VALUES ('sky');
-INSERT INTO atlasses(atlasCode, atlasNr) VALUES ('milleniumbase');
-INSERT INTO atlasses(atlasCode, atlasNr) VALUES ('taki');
-INSERT INTO atlasses(atlasCode, atlasNr) VALUES ('psa');
-INSERT INTO atlasses(atlasCode, atlasNr) VALUES ('torresB',);
-INSERT INTO atlasses(atlasCode, atlasNr) VALUES ('torresBC');
-INSERT INTO atlasses(atlasCode, atlasNr) VALUES ('torresC');
+INSERT INTO atlasses(atlasCode) VALUES ('urano');
+INSERT INTO atlasses(atlasCode) VALUES ('urano_new');
+INSERT INTO atlasses(atlasCode) VALUES ('sky');
+INSERT INTO atlasses(atlasCode) VALUES ('milleniumbase');
+INSERT INTO atlasses(atlasCode) VALUES ('taki');
+INSERT INTO atlasses(atlasCode) VALUES ('psa');
+INSERT INTO atlasses(atlasCode) VALUES ('torresB');
+INSERT INTO atlasses(atlasCode) VALUES ('torresBC');
+INSERT INTO atlasses(atlasCode) VALUES ('torresC');
 
 ALTER TABLE `objects` ADD COLUMN `milleniumbase` VARCHAR(4) NOT NULL AFTER `torresC`;
 
