7532e1aa59c0fe311368517cfd5ba65f532782c7
david <david@b1110c55-7f20-0410-b8ce-a3631e077a91>
2008-11-05 18:57:47 +0000

    baseURL in image references

--- common/layout/tables.php
+++ common/layout/tables.php
@@ -2,12 +2,13 @@
 
 function tableSortHeader($header0, $link0)
 {
+global $baseURL;
 echo "<td valign=\"top\">";         
 echo "<table width=\"100%\">";   
 echo "<tr valign=\top\">";          
 echo "<td>";         
 //echo "<a href=\"" . $link0 . "&amp;sortdirection=asc\"  title=\"" . LangSortOnAsc . "\"><img src=\"../styles/images/up10.gif\" border=\"0\"></a>";
-echo "<a href=\"" . $link0 . "&amp;sortdirection=desc\"  title=\"" . LangSortOnDesc . "\"><img src=\"../styles/images/down10.gif\" border=\"0\"></a>";
+echo "<a href=\"" . $link0 . "&amp;sortdirection=desc\"  title=\"" . LangSortOnDesc . "\"><img src=\"".$baseURL."/styles/images/down10.gif\" border=\"0\"></a>";
 echo "</td>";        
 echo "</tr>";        
 echo "<tr>";		     
@@ -30,12 +31,12 @@ echo "<td>";
 echo "<table>";
 echo "<tr height=\"10px\">";          
 echo "<td width=\10px\" height=\"10px\">";         
-echo "<a href=\"" . $link0 . "&amp;sortdirection=asc\"  title=\"" . LangSortOnAsc . "\"><img src=\"../styles/images/up10.gif\"></a>";
+echo "<a href=\"" . $link0 . "&amp;sortdirection=asc\"  title=\"" . LangSortOnAsc . "\"><img src=\"".$baseURL."/styles/images/up10.gif\"></a>";
 echo "</td>";        
 echo "</tr>";        
 echo "<tr height=\"10px\">";         
 echo "<td width=\10px\" height=\"10px\">";         
-echo "<a href=\"" . $link0 . "&amp;sortdirection=desc\"  title=\"" . LangSortOnDesc . "\"><img src=\"../styles/images/down10.gif\"></a>";
+echo "<a href=\"" . $link0 . "&amp;sortdirection=desc\"  title=\"" . LangSortOnDesc . "\"><img src=\"".$baseURL."/styles/images/down10.gif\"></a>";
 echo "</td>";        
 echo "</tr>";        
 echo "</table>";
