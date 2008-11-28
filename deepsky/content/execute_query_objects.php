19a0a42f7bc9bc41387ce7e7b107fd243b4be65c
david <david@b1110c55-7f20-0410-b8ce-a3631e077a91>
2008-11-28 17:17:35 +0000

    small correction

--- deepsky/content/execute_query_objects.php
+++ deepsky/content/execute_query_objects.php
@@ -45,11 +45,11 @@ if(count($_SESSION['Qobj'])>1) //===============================================
 	echo " &nbsp;-&nbsp;";
   $objUtil->promptWithLink(LangListQueryObjectsMessage14,LangListQueryObjectsMessage15,$baseURL."objectsDetails.pdf?SID=Qobj&amp;sort=".$_SESSION['QobjSort'],LangExecuteQueryObjectsMessage4c);
   echo "&nbsp;-&nbsp";									 
-  echo "<a href=\""$baseURL."objects.argo?SID=Qobj\" target=\"new_window\">".LangExecuteQueryObjectsMessage8."</a>";
+  echo "<a href=\"".$baseURL."objects.argo?SID=Qobj\" target=\"new_window\">".LangExecuteQueryObjectsMessage8."</a>";
 	echo "&nbsp;-&nbsp;";
   if(array_key_exists('listname',$_SESSION)&&$_SESSION['listname']&&$myList)
     echo "<a href=\"".$link."&amp;min=".$min."&amp;addAllObjectsFromQueryToList=true\" title=\"".LangListQueryObjectsMessage5.$_SESSION['listname']."\">".LangListQueryObjectsMessage4."</a>"."&nbsp;-&nbsp;";
-  echo "<a href=\"".baseURL."objects.csv?SID=Qobj\" target=\"new_window\">".LangExecuteQueryObjectsMessage6."</a>";
+  echo "<a href=\"".$baseURL."objects.csv?SID=Qobj\" target=\"new_window\">".LangExecuteQueryObjectsMessage6."</a>";
 	echo "<p>";
 	echo "<a href=\"".$baseURL."index.php?indexAction=query_objects\">".LangExecuteQueryObjectsMessage1."</a>";
 	echo "</div>";
