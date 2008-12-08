47f87c71818232bb29a95d5e67498996d5bb1f89
david <david@b1110c55-7f20-0410-b8ce-a3631e077a91>
2008-12-08 17:59:47 +0000

    Database Class Interface

--- lib/database.php
+++ lib/database.php
@@ -1,26 +1,33 @@
 <?php
 // The database class collects all functions needed to login and logout 
 // from the database.
-//
-// Version 0.1 : 16/08/2004, WDM
-//
-class Database
-{
-  var $databaseId;
+
+interface iDatabase
+{ public function __constructor();
+  public function newlogin();      // Should be removed when code leanup finishes, code should go into the constructor
+	public function login();         // Should be removed when code leanup finishes
+	public function logout();        // Should be removed when code leanup finishes
 	
+	public function execSQL($sql);                                  // Executes an SQL statement, returns nothing
+	public function selectKeyValueArray($sql,$key,$value);          // Executes an SQL statement, inteded to be a select, but it is not checked, it returns an array with the $key field as key and $value field as value of all the records, or an empty array if recordset is empty
+	public function selectRecordset($sql);                          // Executes an SQL statement, it returns the mysql_query object
+	public function selectSingleArray($sql,$name);                  // Executes an SQL statement, inteded to be a select, but it is not checked, it returns an array with the $name field of all the records, or an empty array if recordset is empty
+	public function selectSingleValue($sql,$name,$nullvalue='');    // Executes an SQL statement, inteded to be a select, but it is not checked, it returns the $name field of the first record, or $nullvalue if recordset is empty
+}
+
+class Database implements iDatabase
+{ private $databaseId;
 	public function __constructor()
 	{  //$this->newlogin();
 	}
-	
-  function newlogin()
+	public function newlogin()
   { if(!$this->databaseId)
     { $this->databaseId = mysql_pconnect($GLOBALS['host'], $GLOBALS['user'], $GLOBALS['pass']);
       mysql_select_db($GLOBALS['dbname'], $this->databaseId) or die("Cannot connect to database!");
     }
 		return $this->databaseId;
   }
-	
-  function login()
+	public function login()
   { /*
 	  if(!$this->databaseId)
     { $this->databaseId = mysql_pconnect($host, $user, $pass);
@@ -29,32 +36,16 @@ class Database
 		return $this->databaseId;
 		*/
   }
-
-  function logout()
+	public function logout()
   { //mysql_close($this->databaseId);
 	  //$this->databaseId = NULL;
   }
-	
-	function execSQL($sql)
+  
+  public function execSQL($sql)
 	{ if(!$this->databaseId) {echo "Database connection lost..."; $this->newLogin();}
 		$run = mysql_query($sql) or die(mysql_error());
   }
-	function selectSingleValue($sql,$name,$nullvalue='')
-	{ if(!$this->databaseId) {echo "Database connection lost..."; $this->newLogin();}
-	  $run = mysql_query($sql) or die(mysql_error());
-    $get = mysql_fetch_object($run);
-		if($get) return $get->$name;
-		else     return $nullvalue;
-  }
-	function selectSingleArray($sql,$name)
-	{ if(!$this->databaseId) {echo "Database connection lost..."; $this->newLogin();}
-	  $run = mysql_query($sql) or die(mysql_error());
-    while($get = mysql_fetch_object($run))
-		  $result[]=$get->$name;
-		if($result) return $result;
-		else        return array();
-  }
-	function selectKeyValueArray($sql,$key,$value)
+	public function selectKeyValueArray($sql,$key,$value)
 	{ if(!$this->databaseId) {echo "Database connection lost..."; $this->newLogin();}
 	  $run = mysql_query($sql) or die(mysql_error());
     while($get = mysql_fetch_object($run))
@@ -62,10 +53,25 @@ class Database
 		if($result) return $result;
 		else        return array();
   }
-	function selectRecordset($sql)
+	public function selectRecordset($sql)
 	{ if(!$this->databaseId) {echo "Database connection lost..."; $this->newLogin();}
 	  $run = mysql_query($sql) or die(mysql_error());
 		return $run;
+  }
+  public function selectSingleArray($sql,$name)
+	{ if(!$this->databaseId) {echo "Database connection lost..."; $this->newLogin();}
+	  $run = mysql_query($sql) or die(mysql_error());
+    while($get = mysql_fetch_object($run))
+		  $result[]=$get->$name;
+		if($result) return $result;
+		else        return array();
+  }
+  public function selectSingleValue($sql,$name,$nullvalue='')
+	{ if(!$this->databaseId) {echo "Database connection lost..."; $this->newLogin();}
+	  $run = mysql_query($sql) or die(mysql_error());
+    $get = mysql_fetch_object($run);
+		if($get) return $get->$name;
+		else     return $nullvalue;
   }
 }
 $objDatabase=new Database;
