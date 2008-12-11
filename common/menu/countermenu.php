d611850daf21e347b9391d44ce7a689863e87115
david <david@b1110c55-7f20-0410-b8ce-a3631e077a91>
2008-12-11 08:17:40 +0000

    Counter manu

---
+++ common/menu/countermenu.php
@@ -0,0 +1,32 @@
+<?php
+
+echo "<table cellpadding=\"0\" cellspacing=\"0\" class=\"moduletable\">";
+
+echo "<tr>";
+echo "<th valign=\"top\">".LangCounter."</th>";
+echo "</tr>";
+
+echo "<tr>";
+echo "<td style=\"text-align:right\" >";
+//    <!-- Start of StatCounter Code -->
+echo "<a href=\"http://my6.statcounter.com/project/standard/stats.php?project_id=1347986&guest=1\">";
+echo "<script type=\"text/javascript\" language=\"javascript\">";
+echo "var sc_project=1347986;";
+echo "var sc_invisible=0;";
+echo "var sc_partition=12;";
+echo "var sc_security=\"155f4e3f\";";
+echo "var sc_remove_link=1;";
+echo "</script>";
+echo "<script type=\"text/javascript\" language=\"javascript\" src=\"http://www.statcounter.com/counter/counter.js\">";
+echo "</script>";
+echo "<noscript>";
+echo "<img  src=\"http://c13.statcounter.com/counter.php?sc_project=1347986&amp;java=0&amp;security=155f4e3f&amp;invisible=0\" alt=\"free webpage counters\" border=\"0\">";
+echo "</noscript>";
+echo "</a>";
+//    <!-- End of StatCounter Code -->
+echo "</td>";
+echo "</tr>";
+
+echo "</table>";
+
+?>
