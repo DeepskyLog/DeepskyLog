4bcda244dda7e2bfc9b18ae320a15f44a29477bb
david <david@b1110c55-7f20-0410-b8ce-a3631e077a91>
2008-12-12 15:40:53 +0000

    Detail observer update

--- common/content/view_observer.php
+++ common/content/view_observer.php
@@ -10,82 +10,84 @@ if(!($objObserver->getObserverName($user)))                                     
   throw new Exception(LangViewObserverInexistant); 
 $firstname=$objObserver->getFirstName($user);
 $name=$objObserver->getObserverName($user);
-echo "<div id=\"main\">";
+echo "<div id=\"main\" style=\"text-align:center\">";
 echo "<h2>".$firstname.' '. $name."</h2>";
 $dir = opendir($instDir.'common/observer_pics');
 while(FALSE!==($file=readdir($dir)))
 { if(("." == $file)OR(".."== $file))
     continue;                                                                   // skip current directory and directory above
   if(fnmatch($user. ".gif", $file) || fnmatch($user. ".jpg",$file) || fnmatch($user. ".png", $file))
-    echo "<p><img class=\"viewobserver\" src=\"".$baseURL."common/observer_pics/".$file."\" alt=\"".$firstname."&nbsp;".$name."\"></img></p>";
+    echo "<p ><img class=\"viewobserver\"  src=\"".$baseURL."common/observer_pics/".$file."\" alt=\"".$firstname."&nbsp;".$name."\"></img></p>";
 }
 echo "<form action=\"".$baseURL."index.php?indexAction=change_role\">";
-echo "<table width=\"490\">";
+echo "<table width=\"100%\">";
 if(array_key_exists('admin',$_SESSION)&&($_SESSION['admin']=="yes"))       // admin logged in
 { echo "<tr class=\"type1\">";
-  echo "<td class=\"fieldname\">";
+  echo "<td class=\"fieldname\" style=\"text-align:right\">";
   echo LangChangeAccountField2;
   echo "</td>";
-  echo "<td colspan=\"".sizeof($modules)."\">";
+  echo "<td style=\"text-align:left\">";
   echo "<a href=\"mailto:".$objObserver->getEmail($user)."\">".$objObserver->getEmail($user)."</a>";
   echo "</td>";
 	echo "</tr>";
 }
 echo "<tr class=\"type2\">";
-echo "<td class=\"fieldname\">";
+echo "<td  style=\"text-align:right\">";
 echo LangChangeAccountField3;
 echo "</td>";
-echo "<td colspan=\"".sizeof($modules)."\">";
+echo "<td  style=\"text-align:left\">";
 echo $objObserver->getFirstName($user);
 echo "</td>";
 echo "</tr>";
 echo "<tr class=\"type1\">";
-echo "<td class=\"fieldname\">";
+echo "<td  style=\"text-align:right\">";
 echo LangChangeAccountField4;
 echo "</td>";
-echo "<td>";
+echo "<td style=\"text-align:left\">";
 echo $objObserver->getObserverName($user);
 echo "</td>";
 echo "</tr>";
 echo "<tr class=\"type2\">";
-echo "<td class=\"fieldname\">";
+echo "<td  style=\"text-align:right\">";
 echo LangChangeAccountField7;
 echo "</td>";
-echo "<td colspan=\"" . sizeof($modules) . "\">";
+echo "<td  style=\"text-align:left\">";
 $location_id = $objObserver->getStandardLocation($user);
 $location_name = $objLocation->getLocationName($location_id);
 echo "<a href=\"".$baseURL."index.php?indexAction=detail_location&amp;location=".urlencode($location_id)."\">".$location_name."</a>";
 echo "</td>";
 echo "</tr>";
 echo "<tr class=\"type1\">";
-echo "<td class=\"fieldname\">".LangChangeAccountField8."</td>";
-echo "<td colspan=\"".sizeof($modules)."\">";
+echo "<td  style=\"text-align:right\">".LangChangeAccountField8."</td>";
+echo "<td  style=\"text-align:left\">";
 if($instrumentname=$objInstrument->getInstrumentName($objObserver->getStandardTelescope($user)))
   echo "<a href=\"".$baseURL."index.php?indexAction=detail_instrument&amp;instrument=".urlencode($objObserver->getStandardTelescope($user))."\">".(($instrumentname=="Naked eye")?InstrumentsNakedEye:$instrumentname)."</a>";
 echo "</td>";
 echo "</tr>";
 if($objUtil->checkSessionKey('admin')=="yes")
 { echo "<tr class=\"type2\">";
-  echo "<td class=\"fieldname\">".LangViewObserverRole."</td>";
-  if($objUtil->checkSessionKey('user')&&($user!="admin"))                       // normal user
-  { if(($objObserver->getRole($user)!=RoleWaitlist)&&($user!="admin") )                 // user not in waitlist
-    { echo "<td colspan=\"" . sizeof($modules) . "\">";
-      echo "<select name=\"role\">";
-      echo "<option ".(($objObserver->getRole($user)==RoleAdmin)?"selected=\"selected\"":"")." value=\"0\">".LangViewObserverAdmin."</option>";
-      echo "<option ".(($objObserver->getRole($user)==RoleUser)?"selected=\"selected\"":"")." value=\"1\">".LangViewObserverUser."</option>";
-      echo "<option ".(($objObserver->getRole($user)==RoleCometAdmin)?"selected=\"selected\"":"")." value=\"4\">".LangViewObserverCometAdmin."</option>";
-      echo "<option ".(($objObserver->getRole($user)==RoleWaitlist)?"selected=\"selected\"":"")." value=\"2\">".LangViewObserverWaitlist."</option>";
-      echo "</select>";
-      echo "<input type=\"submit\" name=\"change\" value=\"".LangViewObserverChange."\" />";
-    }
-    elseif($objObserver->getRole($user)==RoleWaitlist)
-      echo(LangViewObserverWaitlist."</td>");
+  echo "<td style=\"text-align:right\">".LangViewObserverRole."</td>";
+  if(($objObserver->getRole($user)!=RoleWaitlist)&&($user!="admin") )                 // user not in waitlist
+  { echo "<td  style=\"text-align:left\">";
+    echo "<select name=\"role\">";
+    echo "<option ".(($objObserver->getRole($user)==RoleAdmin)?"selected=\"selected\"":"")." value=\"0\">".LangViewObserverAdmin."</option>";
+    echo "<option ".(($objObserver->getRole($user)==RoleUser)?"selected=\"selected\"":"")." value=\"1\">".LangViewObserverUser."</option>";
+    echo "<option ".(($objObserver->getRole($user)==RoleCometAdmin)?"selected=\"selected\"":"")." value=\"4\">".LangViewObserverCometAdmin."</option>";
+    echo "<option ".(($objObserver->getRole($user)==RoleWaitlist)?"selected=\"selected\"":"")." value=\"2\">".LangViewObserverWaitlist."</option>";
+    echo "</select>";
+    echo "<input type=\"submit\" name=\"change\" value=\"".LangViewObserverChange."\" />";
   }
+  elseif($objObserver->getRole($user)==RoleWaitlist)
+    echo(LangViewObserverWaitlist."</td>");
   else                                                                          // fixed admin role
   {  echo "<td>".LangViewObserverAdmin."</td>";
   }
   echo "</tr>";
 }
+echo "</table>";
+
+echo "<table width=\"100%\">";
+echo "<tr>";
 $number_of_observations=$objObserver->getNumberOfDsObservations($user);                 // NUMBER OF OBSERVATIONS
 $rank = $objObserver->getRank($user);
 if($rank == 0)
@@ -175,19 +177,19 @@ if(($number_of_observations!=0)||($number_of_comet_observations!=0))
 }
 
 echo "<tr class=\"type1\">";                                                    // Now that all the information is available in the $information array, we can print the table.
-echo "<td class=\"fieldname\">".LangViewObserverNumberOfObservations."</td>";
+echo "<td  style=\"text-align:right\">".LangViewObserverNumberOfObservations."</td>";
 for ($i = 0;$i < count($modules);$i++)
 { echo "<td>".$information[$i][0]."</td>";
 }
 echo "</tr>";
 echo "<tr class=\"type2\">";
-echo "<td class=\"fieldname\">".LangTopObserversHeader4."</td>";
+echo "<td  style=\"text-align:right\">".LangTopObserversHeader4."</td>";
 for ($i = 0;$i < count($modules);$i++)
 { echo "<td>" . $information[$i][1] . "</td>";
 }
 echo "</tr>";
 echo "<tr>";
-echo "<td class=\"type1\">".LangTopObserversHeader6."</td>";
+echo "<td  style=\"text-align:right\">".LangTopObserversHeader6."</td>";
 for ($i = 0;$i < count($modules);$i++)
 { echo "<td>" . $information[$i][2] . "</td>";
 }
@@ -198,7 +200,7 @@ if(is_null($key))
 }
 if ($key !== false)
 { echo "<tr  class=\"type2\">";
-  echo "<td>".LangTopObserversHeader5."</td>";
+  echo "<td style=\"text-align:right\">".LangTopObserversHeader5."</td>";
   for($i=0;$i<count($modules);$i++)
   { echo "<td>";
     if($key==$i)
@@ -210,7 +212,7 @@ if ($key !== false)
     echo "</td>";
   }
   echo "<tr class=\"type1\">";
-  echo "<td class=\"fieldname\">".LangTopObserversHeader5b."</td>";
+  echo "<td  style=\"text-align:right\">".LangTopObserversHeader5b."</td>";
   for($i=0;$i<count($modules);$i++)
   { echo "<td>";
     if ($key == $i)
@@ -222,7 +224,7 @@ if ($key !== false)
     echo "</td>";
   }
   echo "<tr class=\"type2\">";
-  echo "<td class=\"fieldname\">".LangTopObserversHeader5c."</td>";
+  echo "<td  style=\"text-align:right\">".LangTopObserversHeader5c."</td>";
   for($i=0;$i<count($modules);$i++)
   { echo "<td>";
     if ($key == $i)
@@ -235,7 +237,7 @@ if ($key !== false)
   }
 
   echo "<tr class=\"type1\">";
-  echo "<td class=\"fieldname\">".LangTopObserversHeader5d."</td>";
+  echo "<td  style=\"text-align:right\">".LangTopObserversHeader5d."</td>";
   for($i=0;$i<count($modules);$i++)
   { echo "<td>";
     if($key==$i)
@@ -249,7 +251,7 @@ if ($key !== false)
 echo "</tr>";
 }
 echo "<tr class=\"type2\">";
-echo "<td class=\"fieldname\">".LangViewObserverRank."</td>";
+echo "<td  style=\"text-align:right\">".LangViewObserverRank."</td>";
 for($i=0;$i<count($modules);$i++)
 { echo "<td>".$information[$i][4]."</td>";
 }
