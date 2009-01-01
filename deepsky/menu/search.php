2816e5300d4c3910951cee602fcb9cf7f6a8265a
david <david@b1110c55-7f20-0410-b8ce-a3631e077a91>
2009-01-01 12:22:23 +0000

    Layout Overzicht menu

--- deepsky/menu/search.php
+++ deepsky/menu/search.php
@@ -30,7 +30,7 @@ echo "<td>";
 echo "<a href=\"".$baseURL."index.php?indexAction=result_selected_observations&amp;catalog=%&amp;minyear=$lastMinYear&amp;minmonth=$lastMinMonth&amp;minday=$lastMinDay\" class=\"mainlevel\">".LangSearchMenuItem8."</a>";
 echo "</td>";
 echo "</tr>";
-echo "<tr align=\"left\" height=\"25px\">";
+/*echo "<tr align=\"left\" height=\"25px\">";
 echo "<td>";
 echo "<a href=\"".$baseURL."index.php?indexAction=query_observations\" class=\"mainlevel\">".LangSearchMenuItem3."</a>";
 echo "</td>";
@@ -39,7 +39,7 @@ echo "<tr align=\"left\" height=\"25px\">";
 echo "<td>";
 echo "<a href=\"".$baseURL."index.php?indexAction=query_objects\" class=\"mainlevel\">".LangSearchMenuItem5."</a>";
 echo "</td>";
-echo "</tr>";
+echo "</tr>";*/
 echo "<tr align=\"left\" height=\"25px\">";
 echo "<td>";
 echo "<a href=\"".$baseURL."index.php?indexAction=rank_observers\" class=\"mainlevel\">".LangSearchMenuItem6."</a>";
