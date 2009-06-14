e1867685b258dea9c7847e72514cb19a56319997
david <david@b1110c55-7f20-0410-b8ce-a3631e077a91>
2009-06-14 13:56:57 +0000

    Comet View Object XHTML Cleanup

--- comets/content/view_object.php
+++ comets/content/view_object.php
@@ -1,96 +1,28 @@
-<?php
-
-// view_object.php
-// view all information of one object 
-// version 0.5: 2005/09/21, WDM
-
-include_once "lib/cometobjects.php";
-include_once "lib/setup/language.php";
-include_once "lib/cometobservations.php";
-include_once "lib/observers.php";
-include_once "lib/util.php";
-
-$util = new Utils();
-
-$objects = new CometObjects; 
-$observers = new Observers;
-
+<?php // view_object.php - view all information of one object - version 0.5: 2005/09/21, WDM
 if(!$_GET['object']) // no object defined in url 
-{
-   header("Location: ../index.php");
-}  
-
-echo("<div id=\"main\">\n<h2>");
-
-echo (LangViewObjectTitle . "&nbsp;-&nbsp;" . $objects->getName($_GET['object'])); 
-
-echo("</h2>\n<table width=\"490\">\n");
-
-// NAME
-
-echo("<tr>\n
-      <td class=\"fieldname\">");
-
-echo LangViewObjectField1;
-
-echo("</td>\n<td>");
-
-echo($objects->getName($_GET['object']));
-
-echo("</td>\n</tr>\n");
-
-// ICQNAME
-
-if ($objects->getIcqName($_GET['object']))
-{
- echo("<tr>\n
-       <td class=\"fieldname\">");
-
- echo LangNewObjectIcqname;
-
- echo("</td>\n<td>");
-
- echo($objects->getIcqName($_GET['object']));
-
- echo("</td>\n</tr>\n");
-}
-
-echo("</table>\n");
-
+  header("Location: ../index.php");
+echo "<div id=\"main\">";
+$objPresentations->line(array("<h4>".LangViewObjectTitle."&nbsp;-&nbsp;".$objCometObject->getName($_GET['object'])."</h4>"),"L",array(),30);
+echo "<hr />";
+$objPresentations->line(array(LangViewObjectField1,$objCometObject->getName($_GET['object'])),"RL",array(20,80),20);
+if ($objCometObject->getIcqName($_GET['object']))
+  $objPresentations->line(array(LangNewObjectIcqname,$objCometObject->getIcqName($_GET['object'])),"RL",array(20,80),20);
+echo "<hr />";
 // LINK TO OBSERVATIONS OF OBJECT
-
 $observations = new CometObservations();
-
-$obs = $observations->getObservations();
-
-$observation_found = "no";
-
-$queries = array("object" => $objects->getName($_GET['object']));
-$found_observations = $observations->getObservationFromQuery($queries);
-
-if (count($found_observations) > 0)
-{
- $observation_found = "yes";
-}
-
-if($observation_found == "yes")
-{
-   echo("<p><a href=\"".$baseURL."index.php?indexAction=comets_result_query_observations&amp;objectname=" . urlencode($_GET['object']) . 
-        "\">" . LangViewObjectObservations . $objects->getName($_GET['object']) . "</a></p>");
-}
-
+$queries = array("object" => $objCometObject->getName($_GET['object']));
+$content="";
+if (count($observations->getObservationFromQuery($queries)) > 0)
+  $content.="&nbsp;-&nbsp;<a href=\"".$baseURL."index.php?indexAction=comets_result_query_observations&amp;objectname=".urlencode($_GET['object'])."\">" . LangViewObjectObservations . $objCometObject->getName($_GET['object']) . "</a>";
 # extra link to add observation of this object
-
-if($_SESSION['deepskylog_id'])
-{
-
-   $_SESSION['observedobject'] = $_GET['object'];
-   $_SESSION['result'] = $objects->getExactObject($_SESSION['observedobject']);
-   //$_SESSION['observedobject'] = $_SESSION['result'][0]; // use name in database
-   $_SESSION['found'] = "yes";
-   $_SESSION['backlink'] = "validate_search_object.php";
-   echo("<p><a href=\"".$baseURL."index.php?indexAction=comets_add_observation&amp;observedobject=" . urlencode($_GET['object']) . "\">" . LangViewObjectAddObservation . $objects->getName($_GET['object']) . "</a></p>");
+if($loggedUser)
+{ $_SESSION['observedobject'] = $_GET['object'];
+  $_SESSION['result'] = $objCometObject->getExactObject($_SESSION['observedobject']);
+  //$_SESSION['observedobject'] = $_SESSION['result'][0]; // use name in database
+  $_SESSION['found'] = "yes";
+  $_SESSION['backlink'] = "validate_search_object.php";
+  $content.="&nbsp;-&nbsp;<a href=\"".$baseURL."index.php?indexAction=comets_add_observation&amp;observedobject=" . urlencode($_GET['object']) . "\">" . LangViewObjectAddObservation . $objCometObject->getName($_GET['object']) . "</a>";
 }
-
+$objPresentations->line(array(substr($content,13)),"L",array(),20);
 echo("</div>");
 ?>
