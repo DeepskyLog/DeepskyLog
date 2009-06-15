7cb386b2b06f72d0d0d4954355bf8ff9779087ea
david <david@b1110c55-7f20-0410-b8ce-a3631e077a91>
2009-06-15 17:39:01 +0000

    Comet menu cleanup

--- comets/menu/change.php
+++ comets/menu/change.php
@@ -1,52 +1,12 @@
-<?php
-
-// change.php
-// menu which allows the user to add or change things in the database
-
-echo "<table cellpadding=\"0\" cellspacing=\"0\" class=\"moduletable\">\n
-      <tr>\n
-      <th valign=\"top\">\n";
-
-echo (LangChangeMenuTitle);
-
-echo "</th>\n</tr>\n<tr>\n<td>\n
-      <table width=\"100%\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\">\n";
-
-echo "<tr align=\"left\">\n<td>\n<a href=\"".$baseURL."index.php?indexAction=change_account\" class=\"mainlevel\">\n";
-
-echo (LangChangeMenuItem1);
-
-echo "</a>\n</td>\n</tr>\n";
-
-if($_SESSION['deepskylog_id'] != "admin") // admin doesn't have to add a new observation
-{
-   echo "<tr align=\"left\">\n<td>\n<a href=\"".$baseURL."index.php?indexAction=comets_add_observation\" class=\"mainlevel\">";
-
-   echo (LangChangeMenuItem2);
-
-   echo "</a>\n</td>\n</tr>\n";
+<?php // change.php - menu which allows the user to add or change things in the database
+echo "<div class=\"menuDiv\">";
+echo "<p  class=\"menuHead\">".LangChangeMenuTitle."</p>";
+echo "<a href=\"".$baseURL."index.php?indexAction=change_account\" class=\"mainlevel\">".LangChangeMenuItem1."</a><br />";
+if($loggedUser!="admin")
+{ echo "<a href=\"".$baseURL."index.php?indexAction=comets_add_observation\" class=\"mainlevel\">".LangChangeMenuItem2."</a><br />";
 }
-
-echo "<tr align=\"left\">\n<td>\n<a href=\"".$baseURL."index.php?indexAction=add_instrument\" class=\"mainlevel\">";
-
-echo (LangChangeMenuItem3);
-
-echo "</a>\n</td>\n</tr>\n";
-
-echo "<tr align=\"left\">\n<td>\n<a href=\"".$baseURL."index.php?indexAction=add_site\" class=\"mainlevel\">";
-
-echo (LangChangeMenuItem4);
-
-echo "</a>\n</td>\n</tr>\n";
-
-echo "<tr align=\"left\">\n<td>\n<a href=\"".$baseURL."index.php?indexAction=comets_add_object\" class=\"mainlevel\">";
-
-echo (LangChangeMenuItem5);
-
-echo "</a>\n</td>\n</tr>\n";
-
-echo "</table>\n</td>\n
-      </tr>\n
-      </table>\n";
-
+echo "<a href=\"".$baseURL."index.php?indexAction=add_instrument\" class=\"mainlevel\">".LangChangeMenuItem3."</a><br />";
+echo "<a href=\"".$baseURL."index.php?indexAction=add_site\" class=\"mainlevel\">".LangChangeMenuItem4."</a><br />";
+echo "<a href=\"".$baseURL."index.php?indexAction=comets_add_object\" class=\"mainlevel\">".LangChangeMenuItem5."</a><br />";
+echo "</div>";
 ?>
