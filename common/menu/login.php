dac0cca1eb19247f62adb06976f719e580b854ae
david <david@b1110c55-7f20-0410-b8ce-a3631e077a91>
2009-09-03 19:26:03 +0000

    Atlas page key codes

--- common/menu/login.php
+++ common/menu/login.php
@@ -21,7 +21,7 @@ if((!($loggedUser))||($objUtil->checkGetKey('indexAction')=='logout'))
 		echo "<div>";
 		echo "<input type=\"hidden\" name=\"indexAction\" value=\"check_login\" />";
 	  echo "<span class=\"menuInputTitle\">".LangLoginMenuItem1."</span><br />";
-		echo "<input type=\"text\" class=\"inputfield menuInput\" maxlength=\"64\" name=\"deepskylog_id\" size=\"12\" value=\"\" /><br />"; // to add : onkeydown=\"if(event.keyCode==13) {document.getElementById('password').setFocus;}\"
+		echo "<input type=\"text\" class=\"inputfield menuInput\" maxlength=\"64\" name=\"deepskylog_id\" id=\"deepskylog_id\" size=\"12\" value=\"\" /><br />"; // to add : onkeydown=\"if(event.keyCode==13) {document.getElementById('password').setFocus;}\"
 	  echo "<span class=\"menuInputTitle\">".LangLoginMenuItem2."</span><br />";
 		echo "<input type=\"password\" class=\"inputfield menuInput\" maxlength=\"64\" name=\"passwd\" id=\"passwd\" size=\"12\" value=\"\" onkeydown=\"if(event.keyCode==13){this.form.submit();}\" />";
 	  echo "</div>";
--- deepsky/menu/quickpick.php
+++ deepsky/menu/quickpick.php
@@ -17,7 +17,7 @@ echo LangSearch."</p>";
 echo "<input type=\"hidden\" name=\"indexAction\" value=\"quickpick\" />";
 echo "<input type=\"hidden\" name=\"source\"      value=\"quickpick\" />";
 echo "<input type=\"hidden\" name=\"myLanguages\" value=\"true\" />";
-echo "<input type=\"text\" name=\"object\" class=\"inputfield menuInput\" title=\"".LangQuickPickHelp."\" value=\"".((array_key_exists('object',$_GET)&&($_GET['object']!='* '))?$_GET['object']:"")."\" />";
+echo "<input type=\"text\" id=\"quickpickobject\" name=\"object\" class=\"inputfield menuInput\" title=\"".LangQuickPickHelp."\" value=\"".((array_key_exists('object',$_GET)&&($_GET['object']!='* '))?$_GET['object']:"")."\" />";
 if($menuSearch=="expanded")
 {	echo "<input type=\"submit\" name=\"searchObjectQuickPickQuickPick\" class=\"menuButton\" value=\"".LangQuickPickSearchObject."\" />";
 	echo "<input type=\"submit\" name=\"searchObservationsQuickPick\" class=\"menuButton\" value=\"".LangQuickPickSearchObservations."\" />";
