cc73aa68945975c67752528f0b8642b7e529d4e4
wim <wim@b1110c55-7f20-0410-b8ce-a3631e077a91>
2009-09-21 08:50:06 +0000

    Removed the star data

--- scripts/updatev3.3-v3.4final.php
+++ scripts/updatev3.3-v3.4final.php
@@ -5,7 +5,7 @@
  
  print "Database update will add extra fields for double stars. The update will also add fstOFfset to the observers.\n";
 
- // equalBrightness
+/* // equalBrightness
  $sql="ALTER TABLE observations ADD COLUMN equalBrightness int(1) NOT NULL default '-1'";
  $run = mysql_query($sql) or die(mysql_error());
 
@@ -20,7 +20,7 @@
  // component2
  $sql="ALTER TABLE observations ADD COLUMN component2 int(1) NOT NULL default '-1'";
  $run = mysql_query($sql) or die(mysql_error());
-
+*/
  // fstOffset
  $sql="ALTER TABLE observers ADD COLUMN fstOffset float NOT NULL default '0.0'";
  $run = mysql_query($sql) or die(mysql_error());
