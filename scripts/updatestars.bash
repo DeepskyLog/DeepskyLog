29d020f44f9c90f697e12958ac1c74f16fcae3ec
wim <wim@b1110c55-7f20-0410-b8ce-a3631e077a91>
2009-10-09 09:30:12 +0000

    Added script for linux

---
+++ scripts/updatestars.bash
@@ -0,0 +1,27 @@
+#!/bin/bash 
+php updatev3.4-v3.5a.php
+php updatev3.4-v3.5b.php
+php updatev3.4-v3.5c0.php
+php updatev3.4-v3.5c1.php
+php updatev3.4-v3.5c2.php
+php updatev3.4-v3.5c3.php
+php updatev3.4-v3.5c4.php
+php updatev3.4-v3.5c5.php
+php updatev3.4-v3.5c6.php
+php updatev3.4-v3.5c7.php
+php updatev3.4-v3.5c8.php
+php updatev3.4-v3.5c9.php
+php updatev3.4-v3.5c10.php
+php updatev3.4-v3.5c11.php
+php updatev3.4-v3.5c12.php
+php updatev3.4-v3.5c13.php
+php updatev3.4-v3.5c14.php
+php updatev3.4-v3.5c15.php
+php updatev3.4-v3.5c16.php
+php updatev3.4-v3.5c17.php
+php updatev3.4-v3.5c18.php
+php updatev3.4-v3.5c19.php
+php updatev3.4-v3.5c20.php
+php updatev3.4-v3.5c21.php
+php updatev3.4-v3.5c22.php
+php updatev3.4-v3.5c23.php
