f4626e630894347c54ce3df737ee4118dc072935
david <david@b1110c55-7f20-0410-b8ce-a3631e077a91>
2009-12-13 12:42:51 +0000

    Admin Aid part I

--- deepsky/control/admincheckobjects.php
+++ deepsky/control/admincheckobjects.php
@@ -1,11 +1,15 @@
 <?php
 echo "Checking ".($objCnt=count($_SESSION['Qobj']))." objects.<br />";
+echo "<br />";
+echo "Checking objects constellation:<br />";
+echo "<br />";
 $correct=0;
 for($i=0;$i<$objCnt;$i++)
 { if($_SESSION['Qobj'][$i]['objectconstellation']==$objConstellation->getConstellationFromCoordinates($_SESSION['Qobj'][$i]['objectra'],$_SESSION['Qobj'][$i]['objectdecl']))
     $correct++;
   else
-    echo "- ".$_SESSION['Qobj'][$i]['objectname']." constellation ".$_SESSION['Qobj'][$i]['objectconstellation']." should be ".$objConstellation->getConstellationFromCoordinates($_SESSION['Qobj'][$i]['objectra'],$_SESSION['Qobj'][$i]['objectdecl'])."<br />"; 
+    echo "- ".$_SESSION['Qobj'][$i]['objectname']." constellation ".$_SESSION['Qobj'][$i]['objectconstellation']." should be ".$objConstellation->getConstellationFromCoordinates($_SESSION['Qobj'][$i]['objectra'],$_SESSION['Qobj'][$i]['objectdecl']).' '.$_SESSION['Qobj'][$i]['objectra'].' '.$_SESSION['Qobj'][$i]['objectdecl']."<br />"; 
 }
-echo "Correct ".$correct."<br />";
+echo "<br />";
+echo "Correct ".$correct.".<br />";
 ?>
