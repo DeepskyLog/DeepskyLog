c368cd29f5b791ec286766ccc46b22354640589f
wim <wim@b1110c55-7f20-0410-b8ce-a3631e077a91>
2010-06-07 11:59:29 +0000

    Prepare for 4.0 development

--- common/menu/help.php
+++ common/menu/help.php
@@ -2,7 +2,7 @@
 echo "<div class=\"menuDiv\">";
 if($_SESSION['lang']=="nl")
 { echo"<p class=\"menuHead\">Help</p>";
-	echo "<a href=\"http://www.deepskylog.org/wiki/bin/view/Main/DeepskyLogManualNL39\" rel=\"external\">Handleiding</a>";
+	echo "<a href=\"http://www.deepskylog.org/wiki/bin/view/Main/DeepskylogManualNL39\" rel=\"external\">Handleiding</a>";
 }
 echo "</div>";
 ?>
