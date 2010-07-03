ed6834177cb8fe73b566604efac4600f42246ed6
david <david@b1110c55-7f20-0410-b8ce-a3631e077a91>
2010-07-03 16:21:12 +0000

    Objects Sets - solution for timing problem. Typo error.

--- common/content/objectsSets.js
+++ common/content/objectsSets.js
@@ -2,7 +2,7 @@ function generate()
 { i=0;
   while(theobject=document.getElementById('R'+i))
   { var load = window.open('objectsSet.pdf?theobject='+document.getElementById('R'+i).innerHTML+'&theSet='+document.getElementById('R'+i+'Dfov').value,document.getElementById('R'+i).innerHTML);
-    alert('Click ok when '+document.getElementById('R'+i).innerHTML+'is finished.');
+    alert('Click "Ok", only when '+document.getElementById('R'+i).innerHTML+' is finished!');
     i++;
   }
 }
