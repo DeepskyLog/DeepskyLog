5aa5cbad891212e5d023442b055a660126d36edb
david <david@b1110c55-7f20-0410-b8ce-a3631e077a91>
2010-08-29 10:50:00 +0000

    Small update for IE die natuurlijk weer anders moet zijn en geen spatie in een window title toelaat.

--- common/content/objectsSets.js
+++ common/content/objectsSets.js
@@ -21,16 +21,15 @@ function generate()
 }
 function generateOne(i)
 { var load = window.open('objectsSet.pdf?theobject='+urlencode(document.getElementById('R'+i).title)+
-	                                   '&theSet='+urlencode(document.getElementById('R'+i+'Dfov').value)+
-	                                   '&thedsos='+urlencode(document.getElementById('R'+i+'Ddsos').value)+
-	                                   '&thestars='+urlencode(document.getElementById('R'+i+'Dstars').value)+
-	                                   '&thephotos='+urlencode(document.getElementById('R'+i+'Dphotos').value)+
-	                                   '&datapage='+urlencode(document.getElementById('datapage').checked)+
-	                                   '&ephemerides='+urlencode(document.getElementById('ephemerides').checked)+
-	                                   (document.getElementById('indexpage').checked==true?
-	                                   ('&reportlayoutselect='+urlencode(document.getElementById('reportlayoutselect').value)):''),
-                           document.getElementById('R'+i).value
-                           );
+        '&theSet='+urlencode(document.getElementById('R'+i+'Dfov').value)+
+        '&thedsos='+urlencode(document.getElementById('R'+i+'Ddsos').value)+
+        '&thestars='+urlencode(document.getElementById('R'+i+'Dstars').value)+
+        '&thephotos='+urlencode(document.getElementById('R'+i+'Dphotos').value)+
+        '&datapage='+urlencode(document.getElementById('datapage').checked)+
+        '&ephemerides='+urlencode(document.getElementById('ephemerides').checked)+
+        (document.getElementById('indexpage').checked==true?
+        ('&reportlayoutselect='+urlencode(document.getElementById('reportlayoutselect').value)):''),
+        "");
   alert(LangpdfseriesGenerating+document.getElementById('R'+i).value+'.');
   document.getElementById('T'+i).setAttribute('style','background-color:#DDDDDD;');
   document.getElementById('R'+(i+1)).focus();
