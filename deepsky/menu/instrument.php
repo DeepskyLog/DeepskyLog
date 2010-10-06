7e5afbf564185b41809b4bdbe0f5b7155a451ab3
david <david@b1110c55-7f20-0410-b8ce-a3631e077a91>
2010-10-06 11:04:13 +0000

    Small correction on menu selections for instrument and location

--- deepsky/menu/instrument.php
+++ deepsky/menu/instrument.php
@@ -1,25 +1,30 @@
 <?php // instrument.php - menu which allows the user to change its standard instrument
-if($loggedUser) 
-{ echo "<div class=\"menuDivExtended\">";
-	echo "<p   class=\"menuHead\">".LangInstrumentMenuTitle."&nbsp;-&nbsp;"."<a href=\"".$baseURL."index.php?indexAction=add_instrument\">".LangManage."</a>"."</p>";
-  $link=$baseURL."index.php?";
-	reset($_GET);
-	while(list($key,$value)=each($_GET))
-	  $link.=$key.'='.$value.'&amp;';
-	if(array_key_exists('activeTelescopeId',$_GET) && $_GET['activeTelescopeId'])
-  { $objObserver->setObserverProperty($loggedUser,'stdtelescope', $_GET['activeTelescopeId']);
-	  if(array_key_exists('Qobj',$_SESSION))
-		  $_SESSION['Qobj']=$objObject->getObjectVisibilities($_SESSION['Qobj']);
-  }
-	$result=$objInstrument->getSortedInstruments('name',$loggedUser,1);
-  $instr=$objObserver->getObserverProperty($loggedUser,'stdtelescope');	
-	if($result)
-	{ echo "<select name=\"activateTelescope\" class=\"menuField menuDropdown\" onchange=\"location=this.options[this.selectedIndex].value;\">";
-    while(list($key, $value) = each($result))
-		  echo("<option ".(($value==$instr)?"selected=\"selected\"":"")." value=\""  . $link . "&amp;activeTelescopeId=$value\">" . $objInstrument->getInstrumentPropertyFromId($value,'name') . "</option>");
-	  echo "</select>";
+function menuinstrument()
+{ global $baseURL,$loggedUser,
+         $objInstrument,$objObject,$objObserver;
+  if($loggedUser) 
+	{ echo "<div class=\"menuDivExtended\">";
+		echo "<p   class=\"menuHead\">".LangInstrumentMenuTitle."&nbsp;-&nbsp;"."<a href=\"".$baseURL."index.php?indexAction=add_instrument\">".LangManage."</a>"."</p>";
+	  $link=$baseURL."index.php?";
+		reset($_GET);
+		while(list($key,$value)=each($_GET))
+		  $link.=$key.'='.$value.'&amp;';
+		if(array_key_exists('activeTelescopeId',$_GET) && $_GET['activeTelescopeId'])
+	  { $objObserver->setObserverProperty($loggedUser,'stdtelescope', $_GET['activeTelescopeId']);
+		  if(array_key_exists('Qobj',$_SESSION))
+			  $_SESSION['Qobj']=$objObject->getObjectVisibilities($_SESSION['Qobj']);
+	  }
+		$result=$objInstrument->getSortedInstruments('name',$loggedUser,1);
+	  $instr=$objObserver->getObserverProperty($loggedUser,'stdtelescope');	
+		if($result)
+		{ echo "<select name=\"activateTelescope\" class=\"menuField menuDropdown\" onchange=\"location=this.options[this.selectedIndex].value;\">";
+	    while(list($key, $value) = each($result))
+			  echo("<option ".(($value==$instr)?"selected=\"selected\"":"")." value=\""  . $link . "&amp;activeTelescopeId=$value\">" . $objInstrument->getInstrumentPropertyFromId($value,'name') . "</option>");
+		  echo "</select>";
+		}
+		echo "</div>";
+		$link="";
 	}
-	echo "</div>";
-	$link="";
 }
+menuinstrument();
 ?>
--- deepsky/menu/location.php
+++ deepsky/menu/location.php
@@ -1,25 +1,30 @@
 <?php // location.php - menu which allows the user to change its standard location
-if($loggedUser) 
-{ echo "<div class=\"menuDivExtended\">";
-	echo "<p   class=\"menuHead\">".LangLocationMenuTitle."&nbsp;-&nbsp;"."<a href=\"".$baseURL."index.php?indexAction=add_site\">".LangManage."</a>"."</p>";
-  $link=$baseURL."index.php?";
-	reset($_GET);
-	while(list($key,$value)=each($_GET))
-	  $link.=$key.'='.$value.'&amp;';
-	if(array_key_exists('activeLocationId',$_GET) && $_GET['activeLocationId'])
-  { $objObserver->setObserverProperty($loggedUser,'stdlocation', $_GET['activeLocationId']);
-	  if(array_key_exists('Qobj',$_SESSION))
-		  $_SESSION['Qobj']=$objObject->getObjectVisibilities($_SESSION['Qobj']);
-  }
-	$result=$objLocation->getSortedLocations('name',$loggedUser,1);
-  $loc=$objObserver->getObserverProperty($loggedUser,'stdlocation');	
-	if($result)
-	{ echo "<select name=\"activateLocation\" class=\"menuField menuDropdown\" onchange=\"location=this.options[this.selectedIndex].value;\">";
-    while(list($key, $value) = each($result))
-	    echo "<option ".(($value==$loc)?"selected=\"selected\"":"")." value=\"".$link."&amp;activeLocationId=$value\">".$objLocation->getLocationPropertyFromId($value,'name')."</option>";
-	  echo "</select>";
+function menulocation()
+{ global $baseURL,$loggedUser, 
+         $objLocation,$objObject,$objObserver;
+  if($loggedUser)
+  { echo "<div class=\"menuDivExtended\">";
+		echo "<p   class=\"menuHead\">".LangLocationMenuTitle."&nbsp;-&nbsp;"."<a href=\"".$baseURL."index.php?indexAction=add_site\">".LangManage."</a>"."</p>";
+	  $link=$baseURL."index.php?";
+		reset($_GET);
+		while(list($key,$value)=each($_GET))
+		  $link.=$key.'='.$value.'&amp;';
+		if(array_key_exists('activeLocationId',$_GET) && $_GET['activeLocationId'])
+	  { $objObserver->setObserverProperty($loggedUser,'stdlocation', $_GET['activeLocationId']);
+		  if(array_key_exists('Qobj',$_SESSION))
+			  $_SESSION['Qobj']=$objObject->getObjectVisibilities($_SESSION['Qobj']);
+	  }
+		$result=$objLocation->getSortedLocations('name',$loggedUser,1);
+	  $loc=$objObserver->getObserverProperty($loggedUser,'stdlocation');	
+		if($result)
+		{ echo "<select name=\"activateLocation\" class=\"menuField menuDropdown\" onchange=\"location=this.options[this.selectedIndex].value;\">";
+	    while(list($key, $value) = each($result))
+		    echo "<option ".(($value==$loc)?"selected=\"selected\"":"")." value=\"".$link."&amp;activeLocationId=$value\">".$objLocation->getLocationPropertyFromId($value,'name')."</option>";
+		  echo "</select>";
+		}
+		echo "</div>";
+		$link="";
 	}
-	echo "</div>";
-	$link="";
 }
+menulocation();
 ?>
