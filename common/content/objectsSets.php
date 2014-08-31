95e90e8bf38578f75814617518bc87684d948b00
wim <wim@b1110c55-7f20-0410-b8ce-a3631e077a91>
2014-08-31 13:57:10 +0000

    Updated 'maps' to new layout.

--- common/content/objectsSets.php
+++ common/content/objectsSets.php
@@ -38,7 +38,7 @@ function objectsSets()
 	echo LangpdfseriesExplain5.'<br />'.'<br />';		
 	echo LangpdfseriesExplain5b.'<br />'.'<br />';		
 	if(!($MSIE))
-	  echo "<input type=\"button\" value=\"".LangpdfseriesButton."\" onclick=\"generateallonepass(0,".($MSIE?'true':'false').");\"/>";
+	  echo "<input type=\"button\" class=\"btn btn-primary\" value=\"".LangpdfseriesButton."\" onclick=\"generateallonepass(0,".($MSIE?'true':'false').");\"/>";
 	echo "&nbsp;"."<div id='thecounter'> &nbsp; </div>";
 	echo '<br />';	
   echo LangpdfseriesAddDataPage."<input id=\"datapage\" type=\"checkbox\" value=\"\" />";
@@ -51,7 +51,7 @@ function objectsSets()
   else
     echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."<input style=\"visibility:hidden;\" id=\"yearephemerides\" type=\"checkbox\" />";
   echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;".LangpdfseriesAddIndexPage."<input id=\"indexpage\" type=\"checkbox\" onclick=\"if(document.getElementById('indexpage').checked==true) {document.getElementById('reportlayoutselect').style.visibility='visible'; alert('".Langpdfserieschoselayout."');} else document.getElementById('reportlayoutselect').style.visibility='hidden';\" value=\"\" />";
-  echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."<select id=\"reportlayoutselect\" name=\"reportlayoutselect\"  style=\"visibility:hidden;\" >";
+  echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"."<select id=\"reportlayoutselect\" name=\"reportlayoutselect\" class=\"form-control\" style=\"visibility:hidden;\" >";
   $defaults=$objReportLayout->getLayoutListDefault("ReportQueryOfObjects");
   while(list($key, $value) = each($defaults))
     if($value['observerid']=="Deepskylog default")
@@ -69,39 +69,38 @@ function objectsSets()
       echo "<option value=\"".$value['observerid'].': '.$value['reportlayout']."\">".$value['observerid'].': '.$value['reportlayout']."</option>";
   */
   echo "</select>";
-  echo "<hr />";
-  echo "<table>";
+  echo "<table class=\"table table-responsive table-condensed\">";
   echo "<tr>";
   echo "<td> &nbsp; </td>";
-  echo "<td>"."<input id=\"RCB"."all"."\" type=\"button\" value=\"V\" onclick=\"setCheckedValues('fovs',".$k.",document.getElementById('RDfovs').value,'all');
+  echo "<td>"."<input id=\"RCB"."all"."\" class=\"btn btn-primary btn-sm\" type=\"button\" value=\"V\" onclick=\"setCheckedValues('fovs',".$k.",document.getElementById('RDfovs').value,'all');
                                                                                 setCheckedValues('dsos',".$k.",document.getElementById('RDdsos').value,'all');
                                                                                 setCheckedValues('stars',".$k.",document.getElementById('RDstars').value,'all');
                                                                                 setCheckedValues('photos',".$k.",document.getElementById('RDphotos').value,'all');\" />"."</td>";
   echo "<td> &nbsp; </td>";
-  echo "<td>"."<input type=\"text\" id=\"RD"."fovs"."\" value=\"".$fovo." ".$fovl." ".$fovd."\" /><input id=\"RCB"."fovs"."\" type=\"button\" value=\"V\" onclick=\"setCheckedValues('fovs',".$k.",document.getElementById('RDfovs').value);\" />"."</td>";
-  echo "<td>"."<input type=\"text\" id=\"RD"."dsos"."\" value=\"".$dsoso." ".$dsosl." ".$dsosd."\"/><input id=\"RCB"."dsos"."\" type=\"button\" value=\"V\" onclick=\"setCheckedValues('dsos',".$k.",document.getElementById('RDdsos').value);\" />"."</td>";
-  echo "<td>"."<input type=\"text\" id=\"RD"."stars"."\" value=\"".$starso." ".$starsl." ".$starsd."\"/><input id=\"RCB"."stars"."\" type=\"button\" value=\"V\" onclick=\"setCheckedValues('stars',".$k.",document.getElementById('RDstars').value);\" />"."</td>";
-  echo "<td>"."<input type=\"text\" id=\"RD"."photos"."\" value=\"".$foto1." ".$foto2."\"/><input id=\"RCB"."photos"."\" type=\"button\" value=\"V\" onclick=\"setCheckedValues('photos',".$k.",document.getElementById('RDphotos').value);\" />"."</td>";
+  echo "<td>"."<span class=\"form-inline\"><input type=\"text\" class=\"form-control\" id=\"RD"."fovs"."\" value=\"".$fovo." ".$fovl." ".$fovd."\" /><input id=\"RCB"."fovs"."\" class=\"btn btn-primary btn-sm\" type=\"button\" value=\"V\" onclick=\"setCheckedValues('fovs',".$k.",document.getElementById('RDfovs').value);\" />"."</span></td>";
+  echo "<td>"."<span class=\"form-inline\"><input type=\"text\" class=\"form-control\" id=\"RD"."dsos"."\" value=\"".$dsoso." ".$dsosl." ".$dsosd."\"/><input id=\"RCB"."dsos"."\" class=\"btn btn-primary btn-sm\" type=\"button\" value=\"V\" onclick=\"setCheckedValues('dsos',".$k.",document.getElementById('RDdsos').value);\" />"."</span></td>";
+  echo "<td>"."<span class=\"form-inline\"><input type=\"text\" class=\"form-control\" id=\"RD"."stars"."\" value=\"".$starso." ".$starsl." ".$starsd."\"/><input id=\"RCB"."stars"."\" class=\"btn btn-primary btn-sm\" type=\"button\" value=\"V\" onclick=\"setCheckedValues('stars',".$k.",document.getElementById('RDstars').value);\" />"."</span></td>";
+  echo "<td>"."<span class=\"form-inline\"><input type=\"text\" class=\"form-control\" id=\"RD"."photos"."\" value=\"".$foto1." ".$foto2."\"/><input id=\"RCB"."photos"."\" class=\"btn btn-primary btn-sm\" type=\"button\" value=\"V\" onclick=\"setCheckedValues('photos',".$k.",document.getElementById('RDphotos').value);\" />"."</span></td>";
   echo "</tr>";  
   echo "<tr>";
-  echo "<td class=\"bold\">".LangpdfseriesObject."</td>";
-  echo "<td> &nbsp; </td>";
-  echo "<td class=\"bold\">".LangpdfseriesSize."</td>";
-  echo "<td class=\"bold\">".LangpdfseriesFoVs."</td>";
-  echo "<td class=\"bold\">".Langpdfseriesdsos."</td>";
-  echo "<td class=\"bold\">".Langpdfseriesstars."</td>";
-  echo "<td class=\"bold\">".Langpdfseriesphotos."</td>";
+  echo "<th class=\"bold\">".LangpdfseriesObject."</th>";
+  echo "<th> &nbsp; </th>";
+  echo "<th class=\"bold\">".LangpdfseriesSize."</th>";
+  echo "<th class=\"bold\">".LangpdfseriesFoVs."</th>";
+  echo "<th class=\"bold\">".Langpdfseriesdsos."</th>";
+  echo "<th class=\"bold\">".Langpdfseriesstars."</th>";
+  echo "<th class=\"bold\">".Langpdfseriesphotos."</th>";
   
   echo "</tr>";
   for($i=0;$i<$k;$i++)
   { echo "<tr>";
-    echo "<td id=\"T".$i."\">"."<input id=\"R".$i."\" type=\"button\" value=\"".$_SESSION['Qobj'][$i]['showname']."\" title=\"".$_SESSION['Qobj'][$i]['objectname']."\" onclick=\"generateOne(".$i.",".($MSIE?'true':'false').");\"/>"."</td>";
+    echo "<td id=\"T".$i."\">"."<input id=\"R".$i."\" class=\"btn btn-success btn-sm\" type=\"button\" value=\"".$_SESSION['Qobj'][$i]['showname']."\" title=\"".$_SESSION['Qobj'][$i]['objectname']."\" onclick=\"generateOne(".$i.",".($MSIE?'true':'false').");\"/>"."</td>";
   	echo "<td>"."<input id=\"R".$i."CB"."all"."\" type=\"checkbox\" />"."</td>";
     echo "<td id=\"R".$i."Dsize\">".$_SESSION['Qobj'][$i]['objectsize']."</td>";
-    echo "<td>"."<input type=\"text\" ".((($_SESSION['Qobj'][$i]['objectdiam1']/60)>$fovd)?"class=\"textred\"":"")." id=\"R".$i."D"."fovs"."\" value=\"".$fovo." ".$fovl." ".$fovd."\" /><input id=\"R".$i."CB"."fovs"."\" type=\"checkbox\" />"."</td>";
-    echo "<td>"."<input type=\"text\" id=\"R".$i."D"."dsos"."\" value=\"".$dsoso." ".$dsosl." ".$dsosd."\"/><input id=\"R".$i."CB"."dsos"."\" type=\"checkbox\" />"."</td>";
-    echo "<td>"."<input type=\"text\" id=\"R".$i."D"."stars"."\" value=\"".$starso." ".$starsl." ".$starsd."\"/><input id=\"R".$i."CB"."stars"."\" type=\"checkbox\" />"."</td>";
-    echo "<td>"."<input type=\"text\" ".((($_SESSION['Qobj'][$i]['objectdiam1']/60)>15)?"class=\"textred\"":"")."id=\"R".$i."D"."photos"."\" value=\"".$foto1." ".$foto2."\"/><input id=\"R".$i."CB"."photos"."\" type=\"checkbox\" />"."</td>";
+    echo "<td>".((($_SESSION['Qobj'][$i]['objectdiam1']/60)>$fovd)?"<div class=\"form-group has-error has-feedback form-inline\">":"<div class=\"form-group form-inline\">") . "<input type=\"text\" class=\"form-control\" id=\"R".$i."D"."fovs"."\" value=\"".$fovo." ".$fovl." ".$fovd."\" /><input id=\"R".$i."CB"."fovs"."\" type=\"checkbox\" />"."</div></td>";
+    echo "<td>"."<span class=\"form-inline\"><input class=\"form-control\" type=\"text\" id=\"R".$i."D"."dsos"."\" value=\"".$dsoso." ".$dsosl." ".$dsosd."\"/><input id=\"R".$i."CB"."dsos"."\" type=\"checkbox\" />"."</span></td>";
+    echo "<td>"."<span class=\"form-inline\"><input class=\"form-control\" type=\"text\" id=\"R".$i."D"."stars"."\" value=\"".$starso." ".$starsl." ".$starsd."\"/><input id=\"R".$i."CB"."stars"."\" type=\"checkbox\" />"."</span></td>";
+    echo "<td>".((($_SESSION['Qobj'][$i]['objectdiam1']/60)>15)?"<div class=\"form-group has-error has-feedback form-inline\">":"<div class=\"form-group form-inline\">") . "<input class=\"form-control\" type=\"text\" id=\"R".$i."D"."photos"."\" value=\"".$foto1." ".$foto2."\"/><input id=\"R".$i."CB"."photos"."\" type=\"checkbox\" />"."</div></td>";
     echo "</tr>";
   }
   echo "<tr>";
