adedc692c730de314de9336ec7b51c5a2fc98944
wim <wim@b1110c55-7f20-0410-b8ce-a3631e077a91>
2014-09-16 05:33:29 +0000

    Updated 'Search for comets' to new layout.

--- comets/content/setup_objects_query.php
+++ comets/content/setup_objects_query.php
@@ -13,13 +13,16 @@ function setup_objects_query()
 	echo "<div id=\"main\">";
 	echo "<form action=\"".$baseURL."index.php\" method=\"get\"><div>";
 	echo "<input type=\"hidden\" name=\"indexAction\" value=\"comets_result_query_objects\" />";
-	$objPresentations->line(array("<h4>".LangQueryObjectsTitle."</h4>","<input type=\"submit\" name=\"query\" value=\"" . LangQueryObjectsButton1 . "\" />"),"LR",array(70,30),30);
-	echo "<hr />";
+	echo "<h4>".LangQueryObjectsTitle."</h4>";
+	echo "<input type=\"submit\" class=\"btn btn-success pull-right\" name=\"query\" value=\"" . LangQueryObjectsButton1 . "\" />";
+	echo "<br /><hr />";
 	// OBJECT NAME 
-	$content="<input type=\"text\" class=\"inputfield\" maxlength=\"40\" name=\"name\" size=\"40\" value=\"\" />";
-	$objPresentations->line(array(LangQueryObjectsField1,$content),"RL",array(20,80),30);
-	$content="<input type=\"text\" class=\"inputfield\" maxlength=\"40\" name=\"icqname\" size=\"40\" value=\"\" />";
-	$objPresentations->line(array(LangNewObjectIcqname,$content),"RL",array(20,80),30);
+	$content="<input type=\"text\" class=\"form-control\" maxlength=\"40\" name=\"name\" size=\"40\" value=\"\" />";
+	echo "<strong>" . LangQueryObjectsField1 . "</strong>";
+	echo $content;;
+	$content="<input type=\"text\" class=\"form-control\" maxlength=\"40\" name=\"icqname\" size=\"40\" value=\"\" />";
+	echo "<strong>" . LangNewObjectIcqname . "</strong>";
+	echo $content;
 	echo "<hr />";
 	echo "</div></form>";
 	echo "</div>";
