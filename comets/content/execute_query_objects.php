feec46f9c3914c155197ca20b993b109d3649832
wim <wim@b1110c55-7f20-0410-b8ce-a3631e077a91>
2014-09-16 06:07:01 +0000

    Updated results of 'Search for comets' to new layout.

--- comets/content/execute_query_objects.php
+++ comets/content/execute_query_objects.php
@@ -1,127 +1,90 @@
-<?php 
+<?php
 // execute_query_objects.php
 // executes the comet query passed by setup_query_objects.php
-
-global $inIndex,$loggedUser,$objUtil;
-if((!isset($inIndex))||(!$inIndex)) include "../../redirect.php";
-else comets_execute_query_objects();
-
-function comets_execute_query_objects()
-{ global $baseURL,$step,$loggedUser,
-         $objCometObject,$objPresentations,$objUtil,$objCometObservation;
+global $inIndex, $loggedUser, $objUtil;
+if ((! isset ( $inIndex )) || (! $inIndex))
+	include "../../redirect.php";
+else
+	comets_execute_query_objects ();
+function comets_execute_query_objects() {
+	global $baseURL, $step, $loggedUser, $objCometObject, $objPresentations, $objUtil, $objCometObservation;
 	echo "<div id=\"main\">";
-	$content="";
-	if($_GET['name'] || $_GET['icqname']) // at least one search field filled in 
-	{ if (isset($_GET['previous']))
-	  { $prev = $_GET['previous'];
-	  }
-	  else
-	  { $prev = '';
-	  }
-	  $name = $objUtil->checkGetKey('name');
-	  $icqname = $objUtil->checkGetKey('icqname');
-	  // SEARCH ON OBJECT NAME
-	  // SETUP SEARCH QUERY
-	  $query = array("name" => $name, "icqname" => $icqname);
-	  // SORTING
-	  if(isset($_GET['sort'])) // field to sort on given as a parameter in the url
-	  { $sort = $_GET['sort'];
-	  }
-	  else
-	  { $sort = "name"; // standard sort on name
-	  }
-	  // SELECT OBJECTS
-	  $result=$objCometObject->getObjectFromQuery($query, $sort);
-	  if($sort)
-	  { if (isset($_GET['previous']) && $_GET['previous'] == $sort) // reverse sort when pushed twice
-	    { if(sizeof($result) > 0)
-	      { krsort($result);
-	      }
-	      $previous = ""; // reset previous field to sort on
-	    }
-	    else
-	    { $previous = $sort;
-	    }
-	  }
-	  // NUMBER OF PAGES
-	  if ($result)
-	  { $count=0; // counter for altering table colors
-	    $link = $baseURL."index.php?indexAction=comets_result_query_objects&amp;name=" . urlencode($_GET['name']) . "&amp;sort=$sort&amp;previous=" . $prev;
-	    // OUTPUT RESULT
-			if((array_key_exists('steps',$_SESSION))&&(array_key_exists("comObj",$_SESSION['steps'])))
-			  $step=$_SESSION['steps']["comObj"];
-			if(array_key_exists('multiplepagenr',$_GET))
-			  $min = ($_GET['multiplepagenr']-1)*$step;
-			elseif(array_key_exists('multiplepagenr',$_POST))
-			  $min = ($_POST['multiplepagenr']-1)*$step;
-			elseif(array_key_exists('min',$_GET))
-			  $min=$_GET['min'];
-			else
-			  $min = 0;
-			$rank = $objCometObservation->getPopularObservations();
-			list($min, $max, $content) = $objUtil->printNewListHeader3($result, $link, $min, $step, "");
-			$content2=$objUtil->printStepsPerPage3($link,"comObj",$step);
-	    $objPresentations->line(array("<h4>".LangSelectedObjectsTitle."</h4>",$content),"LR",array(60,40),30);
-	 		$objPresentations->line(array($content2),"R",array(100),20);
-	    echo "<hr />";
-	    echo "<table>";
-	    echo "<tr class=\"type3\">";
-	    echo "<td><a href=\"".$baseURL."index.php?indexAction=comets_result_query_objects&amp;name=" . urlencode($_GET['name']) . "&amp;sort=name&amp;previous=$previous\">".LangOverviewObjectsHeader1."</a></td>";
-	    echo "<td><a href=\"".$baseURL."index.php?indexAction=comets_result_query_objects&amp;name=" . urlencode($_GET['name']) . "&amp;sort=icqname&amp;previous=$previous\">".LangNewObjectIcqname."</a></td>";
-	    // Check the number of objects. If there are less than 500 objects, we 
-	    // enable the sorting on seen. 
-	    if (count($result) <= 500)
-	    { echo "<td><a href=\"".$baseURL."index.php?indexAction=comets_result_query_objects&amp;name=" . urlencode($_GET['name']) . "&amp;sort=seen&amp;previous=$previous\">".LangOverviewObjectsHeader7."</a></td>";
-	    }
-	    else
-	    { echo "<td>".LangOverviewObjectsHeader7."</a></td>";
-	    }
-	    echo "</tr>";
-	    while(list($key,$value)=each($result))
-	    { if(($count>=$min)&&($count<$max))
-	      { if ($count % 2)
-	        { $typefield = "class=\"type1\"";
-	        }
-	        else
-	        { $typefield = "class=\"type2\"";
-	        }
-	        // NAME
-	        $name = $value;
-	        $icqname = $objCometObject->getIcqname($objCometObject->getId($value));
-	        // SEEN
-	        $seen = "-";
-	        $see = $objCometObject->getObserved($name);
-	        if ($see == 1) // object has been seen already
-	        { $seen = "<a href=\"".$baseURL."index.php?indexAction=comets_result_query_observations&amp;objectname=" . urlencode($objCometObject->getId($value)) . "\">X</a>";
-	        }
-	        if($loggedUser)
-	        { $see = $objCometObject->getObservedbyUser($name, $loggedUser);
-	          if ($see == 1) // object has been seen by the observer logged in
-	          { $seen = "<a href=\"".$baseURL."index.php?indexAction=comets_result_query_observations&amp;objectname=" . urlencode($objCometObject->getId($value)) . "\">Y</a>";
-	          }
-	        }
-	        echo "<tr $typefield>";
-	        echo "<td><a href=\"".$baseURL."index.php?indexAction=comets_detail_object&amp;object=" . urlencode($objCometObject->getId($value)) . "\">$value</a></td>";
-	        echo "<td>$icqname</td>";
-	        echo "<td class=\"seen\">$seen</td>";
-	        echo "</tr>";
-	      }
-	      $count++; // increase line counter
-	    }   
-	    $_SESSION['object_query'] = $result;
-	    echo "</table>";
-	    echo "<hr />";
-	  }
-	  else // no results found
-	  { $objPresentations->line(array("<h4>".LangSelectedObjectsTitle."</h4>"),"L",array(),30);
-	    echo "<hr />";
-	  	echo LangExecuteQueryObjectsMessage2;
-	  }
-	}
-	else // no query fields filled in
-	{ $objPresentations->line(array("<h4>".LangSelectedObjectsTitle."</h4>",$content),"LR",array(60,40),30);
-	  echo "<hr />";
-	  echo LangExecuteQueryObjectsMessage3;
+	if ($_GET ['name'] || $_GET ['icqname']) 	// at least one search field filled in
+	{
+		$name = $objUtil->checkGetKey ( 'name' );
+		$icqname = $objUtil->checkGetKey ( 'icqname' );
+		// SEARCH ON OBJECT NAME
+		// SETUP SEARCH QUERY
+		$query = array (
+				"name" => $name,
+				"icqname" => $icqname 
+		);
+		$sort = "name"; // standard sort on name
+		                // SELECT OBJECTS
+		$result = $objCometObject->getObjectFromQuery ( $query, $sort );
+		// NUMBER OF PAGES
+		if ($result) {
+			$count = 0; // counter for altering table colors
+			$link = $baseURL . "index.php?indexAction=comets_result_query_objects&amp;name=" . urlencode ( $_GET ['name'] );
+			// OUTPUT RESULT
+			$rank = $objCometObservation->getPopularObservations ();
+			echo "<h4>" . LangSelectedObjectsTitle . "</h4>";
+			echo "<hr />";
+			echo "<table class=\"table sort-tablecometobjects table-condensed table-striped table-hover tablesorter custom-popup\">";
+			echo "<thead><tr>";
+			echo "<th><a href=\"" . $baseURL . "index.php?indexAction=comets_result_query_objects&amp;name=" . urlencode ( $_GET ['name'] ) . "\">" . LangOverviewObjectsHeader1 . "</a></th>";
+			echo "<th><a href=\"" . $baseURL . "index.php?indexAction=comets_result_query_objects&amp;name=" . urlencode ( $_GET ['name'] ) . "\">" . LangNewObjectIcqname . "</a></th>";
+			// Check the number of objects. If there are less than 500 objects, we
+			// enable the sorting on seen.
+			if (count ( $result ) <= 500) {
+				echo "<th><a href=\"" . $baseURL . "index.php?indexAction=comets_result_query_objects&amp;name=" . urlencode ( $_GET ['name'] ) . "\">" . LangOverviewObjectsHeader7 . "</a></th>";
+			} else {
+				echo "<th>" . LangOverviewObjectsHeader7 . "</a></th>";
+			}
+			echo "</tr></thead>";
+			while ( list ( $key, $value ) = each ( $result ) ) {
+				// NAME
+				$name = $value;
+				$icqname = $objCometObject->getIcqname ( $objCometObject->getId ( $value ) );
+				// SEEN
+				$seen = "-";
+				$see = $objCometObject->getObserved ( $name );
+				if ($see == 1) 				// object has been seen already
+				{
+					$seen = "<a href=\"" . $baseURL . "index.php?indexAction=comets_result_query_observations&amp;objectname=" . urlencode ( $objCometObject->getId ( $value ) ) . "\">X</a>";
+				}
+				if ($loggedUser) {
+					$see = $objCometObject->getObservedbyUser ( $name, $loggedUser );
+					if ($see == 1) 					// object has been seen by the observer logged in
+					{
+						$seen = "<a href=\"" . $baseURL . "index.php?indexAction=comets_result_query_observations&amp;objectname=" . urlencode ( $objCometObject->getId ( $value ) ) . "\">Y</a>";
+					}
+				}
+				echo "<tr>";
+				echo "<td><a href=\"" . $baseURL . "index.php?indexAction=comets_detail_object&amp;object=" . urlencode ( $objCometObject->getId ( $value ) ) . "\">$value</a></td>";
+				echo "<td>$icqname</td>";
+				echo "<td class=\"seen\">$seen</td>";
+				echo "</tr>";
+				$count ++; // increase line counter
+			}
+			$_SESSION ['object_query'] = $result;
+			echo "</table>";
+			echo $objUtil->addTablePager ( "cometobjects" );
+			
+			echo $objUtil->addTableJavascript ( "cometobjects" );
+			
+			echo "<hr />";
+		} else 		// no results found
+		{
+			echo "<h4>" . LangSelectedObjectsTitle . "</h4>";
+			echo "<hr />";
+			echo LangExecuteQueryObjectsMessage2;
+		}
+	} else 	// no query fields filled in
+	{
+		echo "<h4>" . LangSelectedObjectsTitle . "</h4>";
+		echo "<hr />";
+		echo LangExecuteQueryObjectsMessage3;
 	}
 	echo "</div>";
 }
--- comets/content/view_object.php
+++ comets/content/view_object.php
@@ -1,75 +1,81 @@
-<?php 
+<?php
 // view_object.php
 // view all information of one object
-
-global $inIndex,$loggedUser,$objUtil;
-if((!isset($inIndex))||(!$inIndex)) include "../../redirect.php";
-else view_object();
-
-function view_object()
-{ global $baseURL,$loggedUser,
-         $objObserver,$objPresentations,$objCometObject;
-	if(!$_GET['object']) // no object defined in url 
-	  header("Location: ../index.php");
+global $inIndex, $loggedUser, $objUtil;
+if ((! isset ( $inIndex )) || (! $inIndex))
+	include "../../redirect.php";
+else
+	view_object ();
+function view_object() {
+	global $baseURL, $loggedUser, $objObserver, $objPresentations, $objCometObject;
+	if (! $_GET ['object']) // no object defined in url
+		header ( "Location: ../index.php" );
 	echo "<div id=\"main\">";
 	
 	// Let's test for the observer... If cometadministrator (or normal administrator), we can change the object)
 	$admin = false;
 	
 	// Check if there is an observer
-	if(array_key_exists('deepskylog_id', $_SESSION)) {
-	  // Check if this observer is cometadministrator
-	  if  ($objObserver->getObserverProperty($_SESSION['deepskylog_id'], "role") == RoleCometAdmin ||
-	        $objObserver->getObserverProperty($_SESSION['deepskylog_id'], "role") == RoleAdmin) {
-	          $admin = true;
-	  } 
+	if (array_key_exists ( 'deepskylog_id', $_SESSION )) {
+		// Check if this observer is cometadministrator
+		if ($objObserver->getObserverProperty ( $_SESSION ['deepskylog_id'], "role" ) == RoleCometAdmin || $objObserver->getObserverProperty ( $_SESSION ['deepskylog_id'], "role" ) == RoleAdmin) {
+			$admin = true;
+		}
 	}
 	
 	if ($admin) {
-	  echo "<form action=\"".$baseURL."index.php?indexAction=comets_validate_change_object\" method=\"post\"><div>";
-	  echo "<input type=\"hidden\" name=\"object\" value=\"" . $_GET['object'] . "\" />";
-	  $content="<input type=\"submit\" name=\"newobject\" value=\"" . LangChangeAccountButton . "\" />";
-	  $objPresentations->line(array("<h4>".LangChangeObject. " " . $objCometObject->getName($_GET['object']) . "</h4>",$content),"LR",array(60,40),30);
-	  echo "<hr />";
-	  $content="<input type=\"text\" required class=\"inputfield requiredField\" maxlength=\"40\" name=\"name\" size=\"40\" value=\"" . 
-	    $objCometObject->getName($_GET['object']) . "\" />";
-	  $objPresentations->line(array(LangViewObjectField1."&nbsp;*",$content),"RL",array(20,80),30,array("fieldname"));
-	  
-	  if ($objCometObject->getIcqName($_GET['object'])) {
-	    $icqname = $objCometObject->getIcqName($_GET['object']);
-	  } else {
-	    $icqname = "";
-	  }
-	  $content="<input type=\"text\" required class=\"inputfield requiredField\" maxlength=\"40\" name=\"icqname\" size=\"40\" value=\"" . $icqname . "\" />";
-	  $objPresentations->line(array(LangNewObjectIcqname."&nbsp;*",$content),"RL",array(20,80),30,array("fieldname"));
-	  echo "<hr />";
-	  echo "</div></form>";
+		echo "<form action=\"" . $baseURL . "index.php?indexAction=comets_validate_change_object\" method=\"post\"><div>";
+		echo "<input type=\"hidden\" name=\"object\" value=\"" . $_GET ['object'] . "\" />";
+		$content = "<input type=\"submit\" class=\"btn btn-successpull-right\" name=\"newobject\" value=\"" . LangChangeAccountButton . "\" />";
+		echo "<h4>" . LangChangeObject . " " . $objCometObject->getName ( $_GET ['object'] ) . "</h4>";
+		echo $content;
+		echo "<br /><hr />";
+		$content = "<input type=\"text\" required class=\"form-control\" maxlength=\"40\" name=\"name\" size=\"40\" value=\"" . $objCometObject->getName ( $_GET ['object'] ) . "\" />";
+		echo "<strong>" . LangViewObjectField1 . "&nbsp;*</strong>";
+		echo $content;
+		
+		if ($objCometObject->getIcqName ( $_GET ['object'] )) {
+			$icqname = $objCometObject->getIcqName ( $_GET ['object'] );
+		} else {
+			$icqname = "";
+		}
+		$content = "<input type=\"text\" required class=\"form-control\" maxlength=\"40\" name=\"icqname\" size=\"40\" value=\"" . $icqname . "\" />";
+		echo "<strong>" . LangNewObjectIcqname . "&nbsp;*</strong>";
+		echo $content;
+		;
+		echo "<hr />";
+		echo "</div></form>";
 	} else {
-	  $objPresentations->line(array("<h4>".LangViewObjectTitle."&nbsp;-&nbsp;".$objCometObject->getName($_GET['object'])."</h4>"),"L",array(),30);
-	  echo "<hr />";
-	  
-	  $objPresentations->line(array(LangViewObjectField1,$objCometObject->getName($_GET['object'])),"RL",array(20,80),20);
-	  if ($objCometObject->getIcqName($_GET['object']))
-	    $objPresentations->line(array(LangNewObjectIcqname,$objCometObject->getIcqName($_GET['object'])),"RL",array(20,80),20);
-	  echo "<hr />";
+		echo "<h4>" . LangViewObjectTitle . "&nbsp;-&nbsp;" . $objCometObject->getName ( $_GET ['object'] ) . "</h4>";
+		echo "<hr />";
+		
+		echo "<strong>" . LangViewObjectField1 . "</strong>";
+		echo "<input type=\"text\" disabled class=\"form-control\" maxlength=\"40\" name=\"name\" size=\"40\" value=\"" . $objCometObject->getName ( $_GET ['object'] ) . "\" />";
+		if ($objCometObject->getIcqName ( $_GET ['object'] )) {
+			echo "<strong>" . LangNewObjectIcqname . "</strong>";
+			echo "<input type=\"text\" disabled class=\"form-control\" maxlength=\"40\" name=\"icqname\" size=\"40\" value=\"" . $objCometObject->getIcqName ( $_GET ['object'] ) . "\" />";
+		}
+		echo "<hr />";
 	}
 	
 	// LINK TO OBSERVATIONS OF OBJECT
-	$observations = new CometObservations();
-	$queries = array("object" => $objCometObject->getName($_GET['object']));
-	$content="";
-	if (count($observations->getObservationFromQuery($queries)) > 0)
-	  $content.="&nbsp;-&nbsp;<a href=\"".$baseURL."index.php?indexAction=comets_result_query_observations&amp;objectname=".urlencode($_GET['object'])."\">" . LangViewObjectObservations . " " . $objCometObject->getName($_GET['object']) . "</a>";
-	# extra link to add observation of this object
-	if($loggedUser)
-	{ $_SESSION['observedobject'] = $_GET['object'];
-	  $_SESSION['result'] = $objCometObject->getExactObject($_SESSION['observedobject']);
-	  //$_SESSION['observedobject'] = $_SESSION['result'][0]; // use name in database
-	  $_SESSION['found'] = "yes";
-	  $_SESSION['backlink'] = "validate_search_object.php";
-	  $content.="&nbsp;-&nbsp;<a href=\"".$baseURL."index.php?indexAction=comets_add_observation&amp;observedobject=" . urlencode($_GET['object']) . "\">" . LangViewObjectAddObservation ."&nbsp;". $objCometObject->getName($_GET['object']) . "</a>";
+	$observations = new CometObservations ();
+	$queries = array (
+			"object" => $objCometObject->getName ( $_GET ['object'] ) 
+	);
+	$content = "";
+	if (count ( $observations->getObservationFromQuery ( $queries ) ) > 0)
+		$content .= "&nbsp;&nbsp;<a class=\"btn btn-success\" href=\"" . $baseURL . "index.php?indexAction=comets_result_query_observations&amp;objectname=" . urlencode ( $_GET ['object'] ) . "\">" . LangViewObjectObservations . " " . $objCometObject->getName ( $_GET ['object'] ) . "</a>";
+		// extra link to add observation of this object
+	if ($loggedUser) {
+		$_SESSION ['observedobject'] = $_GET ['object'];
+		$_SESSION ['result'] = $objCometObject->getExactObject ( $_SESSION ['observedobject'] );
+		// $_SESSION['observedobject'] = $_SESSION['result'][0]; // use name in database
+		$_SESSION ['found'] = "yes";
+		$_SESSION ['backlink'] = "validate_search_object.php";
+		$content .= "&nbsp;&nbsp;<a class=\"btn btn-success\" href=\"" . $baseURL . "index.php?indexAction=comets_add_observation&amp;observedobject=" . urlencode ( $_GET ['object'] ) . "\">" . LangViewObjectAddObservation . "&nbsp;" . $objCometObject->getName ( $_GET ['object'] ) . "</a>";
 	}
-	$objPresentations->line(array(substr($content,13)),"L",array(),20);
-	echo("</div>");
+	echo $content; 
+	echo ("</div>");
 }
 ?>
