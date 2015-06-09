31e5affa5be3ffc0323526c38315770a781ca5a7
Wim De Meester <deepskywim@gmail.com>
2015-06-09 21:17:26 +0200

    Add TODOs.
    293: In the 'Manage list' page, it is not clear what the 'Add' button
    does. 
    
    Task-Url: http://github.com/DeepskyLog/DeepskyLog/issues/issue/293

---
+++ deepsky/content/new_list.php
@@ -0,0 +1,6 @@
+<?php
+  // TODO: Make it possible to add a new list.
+  // TODO: Make it easy to change from public to private list.
+  // TODO: Show a list of all your observing lists.
+  echo "TEST";
+?>
