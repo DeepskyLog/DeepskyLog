88cfe227a68d6f692044ecc1f5f9a23b030dc986
Wim De Meester <deepskywim@gmail.com>
2015-06-19 13:42:50 +0200

    Make the menu 'My observing lists' work.

--- deepsky/content/new_list.php
+++ deepsky/content/new_list.php
@@ -1,6 +1,4 @@
 <?php
-  // TODO: REMOVE THIS FILE
   // TODO: Make it easy to change from public to private list.
-  // TODO: Show a list of all your observing lists -> This should be in 'View'->'My observing lists'
   echo "TEST";
 ?>
