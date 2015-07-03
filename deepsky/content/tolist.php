ba75765a00328931cb0e0b06a7800e3fe5f25a87
Wim De Meester <deepskywim@gmail.com>
2015-07-03 12:50:33 +0200

    Move button to change the name of a list from <a href="..."> to <button ...>

--- deepsky/content/tolist.php
+++ deepsky/content/tolist.php
@@ -36,8 +36,11 @@ function tolist() {
 		
 		if ($myList) {
 			$content1 = "<a class=\"btn btn-danger\" href=\"" . $baseURL . "index.php?indexAction=listaction&amp;removeList=removeList\"><span class=\"glyphicon glyphicon-remove\" aria-hidden=\"true\"></span>&nbsp;" . LangToListMyListsRemove . "</a>  ";
-			// TODO: Make renaming the list work!
-			$content1 .= "<a class=\"btn btn-warning\" href=\"" . $baseURL . "index.php?indexAction=listaction&amp;removeList=removeList\"><span class=\"glyphicon glyphicon-pencil\" aria-hidden=\"true\"></span>&nbsp;" . LangToListRename . "</a>  ";
+			// TODO: Make renaming the list work! We should move the changeListName modal to index.php
+			$content1 .= "<button type=\"button\" title=\"" . LangChangeName . "\" class=\"btn btn-warning\" data-toggle=\"modal\" data-target=\"#changeListName" . str_replace ( ' ', '_', str_replace ( ':', '_', $listname ) ) . "\" >
+							<span class=\"glyphicon glyphicon-pencil\"></span>&nbsp;" . LangToListRename . "
+                      	  </button>&nbsp;";
+				
 			// Add a button to change from private to public or vice-versa
 			// TODO: Make this button work!
 			if ($objList->isPublic($listname_ss)) {
@@ -46,6 +49,7 @@ function tolist() {
 				$content1 .= "<a class=\"btn btn-warning\" href=\"" . $baseURL . "index.php?indexAction=listaction&amp;removeList=removeList\"><span class=\"glyphicon glyphicon-share\" aria-hidden=\"true\"></span>&nbsp;Make public</a>  ";
 			}
 			// Add a button to create a new list
+			// TODO: Make this button work!
 			$content1 .= "<a class=\"btn btn-success pull-right\" href=\"" . $baseURL . "index.php?indexAction=listaction&amp;removeList=removeList\"><span class=\"glyphicon glyphicon-plus\" aria-hidden=\"true\"></span>&nbsp;Add new list</a>  ";
 				
 			$content2 = "<a class=\"btn btn-success\" href=\"" . $baseURL . "index.php?indexAction=import_csv_list\">" . LangToListImport . "</a>  ";
