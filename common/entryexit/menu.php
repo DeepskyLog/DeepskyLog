a10c1bda9958f5de9f9ea687dc70f755db276cb3
Wim De Meester <deepskywim@gmail.com>
2015-09-11 21:55:04 +0200

    Make the top and left menu fixed. This fixes issue #355.

--- common/entryexit/menu.php
+++ common/entryexit/menu.php
@@ -8,12 +8,12 @@ else menu();
 function menu()
 { global $loggedUser,$instDir,$inIndex;
 
-  echo "  <div class=\"col-sm-2\">
-           <ul id=\"sidebar\" class=\"nav nav-stacked\">";
+  echo "  <div class=\"col-sm-2 sidebar-outer\">
+           <ul class=\"nav nav-stacked sidebar\">";
 
 	if($loggedUser) {                                                                            // LOGGED IN
 	} else
-	  require_once $instDir.'common/menu/languagemenu.php';                                         // LANGUAGE MENU   
+	  require_once $instDir.'common/menu/languagemenu.php';                                         // LANGUAGE MENU
 	require_once $instDir.$_SESSION['module'].'/menu/quickpick.php';                                // QUICKPICK MENU
   require_once $instDir.'common/menu/moon.php';                                                   // MOON MENU
 
