ecd0550c5463e67c001fcb7174b8750548eb0cc0
Wim De Meester <deepskywim@gmail.com>
2016-01-18 00:49:25 +0100

    Add buttons at the bottom of the forms for the entries in the overview menu.

--- common/content/view_eyepieces.php
+++ common/content/view_eyepieces.php
@@ -14,7 +14,7 @@ function new_eyepiece() {
 	echo "<hr />";
 	echo "<form role=\"form\" action=\"" . $baseURL . "index.php\" method=\"post\"><div>";
 	echo "<input type=\"hidden\" name=\"indexAction\" value=\"add_eyepiece\" />";
-	echo "<input type=\"submit\" class=\"btn btn-primary pull-right\" name=\"add\" value=\"" . LangAddEyepieceButton . "\" />&nbsp;";
+	echo "<input type=\"submit\" class=\"btn btn-success pull-right\" name=\"add\" value=\"" . LangAddEyepieceButton . "\" />&nbsp;";
 	echo "</div>";
 	echo "</form>";
 	$objEyepiece->showEyepiecesObserver ();
