a090d0d7eff904c2aa3b6755347fdc76563ffc97
Wim De Meester <wim.demeester@ster.kuleuven.be>
2016-02-05 16:09:09 +0100

    Bump version number.

--- lib/setup/vars.php
+++ lib/setup/vars.php
@@ -6,7 +6,7 @@ if ((! isset ( $inIndex )) || (! $inIndex))
 	include "../../redirect.php";
 
 $copyright = "&copy;2004&nbsp;-&nbsp;2016";
-$versionInfo = "5.0.34";
+$versionInfo = "5.0.35";
 $copyrightInfo = $copyright . ",&nbsp;DeepskyLog developers";
 
 define ( "AtlasOverviewZoom", 17 );
