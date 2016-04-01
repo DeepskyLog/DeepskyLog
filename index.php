22d140b20e2bbc13475331caf19a66104a1effe9
Wim De Meester <wim.demeester@ster.kuleuven.be>
2016-04-01 22:40:50 +0200

    Show toast message for adding or deleting an object from a list.

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 // index.php
 // main entrance to DeepskyLog
 try {
-	global $loggedUser;
+	global $loggedUser, $toastMessage;
 	$inIndex = true;
 	$language = "nl";
 	if (! array_key_exists ( 'indexAction', $_GET ) && array_key_exists ( 'indexAction', $_POST ))
-		$_GET ['indexAction'] = $_POST ['indexAction'];
+	$_GET ['indexAction'] = $_POST ['indexAction'];
 	date_default_timezone_set ( 'UTC' );
 	require_once 'common/entryexit/globals.php'; // Includes of all classes and assistance files
 	require_once 'common/entryexit/preludes.php'; // Includes of all classes and assistance files
@@ -26,29 +26,29 @@ try {
 
 	// Container-fluid makes the container the full width of the screen.
 	echo "<div class=\"container-fluid\">
-         <div class=\"row\">";
+	<div class=\"row\">";
 
 	require_once 'common/entryexit/menu.php';
 	echo "   <div class=\"col-sm-10 move\">";
 	require_once $includeFile;
 	echo "    </div>
-  		     </div>
-  		    </div>";
+	</div>
+	</div>";
 
 	echo "<div class=\"navbar navbar-default navbar-bottom\">
-  		   <div class=\"container-fluid\">
-  		    <div class=\"collapse navbar-collapse\" id=\"bs-example-navbar-collapse-1\">
-  		     <ul class=\"nav navbar-nav navbar-left\">
-  		  		<p class=\"navbar-text\">" . $copyrightInfo . " - " . $vvsInfo . $dslInfo . $versionInfo . " - " . $objectInfo . "</p>
-  		     </ul>";
+	<div class=\"container-fluid\">
+	<div class=\"collapse navbar-collapse\" id=\"bs-example-navbar-collapse-1\">
+	<ul class=\"nav navbar-nav navbar-left\">
+	<p class=\"navbar-text\">" . $copyrightInfo . " - " . $vvsInfo . $dslInfo . $versionInfo . " - " . $objectInfo . "</p>
+	</ul>";
 
 	// Add fork me on GitHub button
 	echo "<ul class=\"nav navbar-nav navbar-right\">";
 
 	echo "<li><a href=\"https://github.com/DeepskyLog/DeepskyLog/fork\" rel=\"external\">
-  		      <img src=\"" . $baseURL . "images/GitHub.png\" alt=\"Fork me on GitHub\"/>
-		    </a>
-  		</li>";
+	<img src=\"" . $baseURL . "images/GitHub.png\" alt=\"Fork me on GitHub\"/>
+	</a>
+	</li>";
 
 	// Add logo for oal
 	echo "<li><a href=\"http://groups.google.com/group/openastronomylog\" rel=\"external\">";
@@ -66,8 +66,8 @@ try {
 
 	echo "</ul>";
 	echo "  </div>
-  		   </div>
-  		  </div>";
+	</div>
+	</div>";
 } catch ( Exception $e ) {
 	$entryMessage .= "<p>DeepskyLog encountered a problem. Could you please report it to the Developers?</p>";
 	$entryMessage .= "<p>Report problem with error message: " . $e->getMessage () . "</p>";
@@ -88,58 +88,82 @@ echo "</script>";
 // Modal to make a new list
 if ($_SESSION ['module'] == 'deepsky' && $loggedUser) {
 	echo "<div class=\"modal fade\" id=\"addList\">
-        <div class=\"modal-dialog\">
-         <div class=\"modal-content\">
-          <div class=\"modal-header\">
-           <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>
-           <h4 class=\"modal-title\">" . LangAddObservingList . "</h4>
-          </div>
-          <div class=\"modal-body\">
-           <!-- Ask for the name of the list. -->
-           <h1 class=\"text-center login-title\">" . LangNameNewList . "</h1>
-            <form action=\"".$baseURL."index.php?indexAction=listaction\">
-             <input type=\"hidden\" name=\"indexAction\" value=\"listaction\" />
-             <input type=\"text\" name=\"addlistname\" class=\"form-control\" required autofocus>
-             <br /><br />
-             <input type=\"checkbox\" name=\"PublicList\" value=\"1\" />&nbsp;" . LangToListPublic . "
-            </div>
-            <div class=\"modal-footer\">
-            <button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Close</button>
-            <input class=\"btn btn-success\" type=\"submit\" name=\"addList\" value=\"" . LangAddList . "\" />
-		   </form>
-          </div>
-         </div><!-- /.modal-content -->
-        </div><!-- /.modal-dialog -->
-       </div><!-- /.modal -->";
+	<div class=\"modal-dialog\">
+	<div class=\"modal-content\">
+	<div class=\"modal-header\">
+	<button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\"><span aria-hidden=\"true\">&times;</span></button>
+	<h4 class=\"modal-title\">" . LangAddObservingList . "</h4>
+	</div>
+	<div class=\"modal-body\">
+	<!-- Ask for the name of the list. -->
+	<h1 class=\"text-center login-title\">" . LangNameNewList . "</h1>
+	<form action=\"".$baseURL."index.php?indexAction=listaction\">
+	<input type=\"hidden\" name=\"indexAction\" value=\"listaction\" />
+	<input type=\"text\" name=\"addlistname\" class=\"form-control\" required autofocus>
+	<br /><br />
+	<input type=\"checkbox\" name=\"PublicList\" value=\"1\" />&nbsp;" . LangToListPublic . "
+	</div>
+	<div class=\"modal-footer\">
+	<button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Close</button>
+	<input class=\"btn btn-success\" type=\"submit\" name=\"addList\" value=\"" . LangAddList . "\" />
+	</form>
+	</div>
+	</div><!-- /.modal-content -->
+	</div><!-- /.modal-dialog -->
+	</div><!-- /.modal -->";
 }
 
 if (isset ( $entryMessage ) && $entryMessage) { // dispays $entryMessage if any
 	echo "<div class=\"modal fade\" id=\"errorModal\" tabindex=\"-1\">
-          <div class=\"modal-dialog\">
-            <div class=\"modal-content\">
- 			        <div class=\"modal-header\">
- 			          <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-hidden=\"true\">&times;</button>
- 			          <h4 class=\"modal-title\" id=\"myModalLabel\">DeepskyLog</h4>
- 			        </div>
-  		        <div class=\"modal-body\">" . $entryMessage . "
-  	  	      </div>
-            </div>
-          </div>
-        </div>";
+	<div class=\"modal-dialog\">
+	<div class=\"modal-content\">
+	<div class=\"modal-header\">
+	<button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-hidden=\"true\">&times;</button>
+	<h4 class=\"modal-title\" id=\"myModalLabel\">DeepskyLog</h4>
+	</div>
+	<div class=\"modal-body\">" . $entryMessage . "
+	</div>
+	</div>
+	</div>
+	</div>";
 
 	echo "<script type=\"text/javascript\">";
 	echo "$(document).ready(function() {
-          $('#errorModal').modal('show')
-				});";
+		$('#errorModal').modal('show')
+	});";
 	echo "</script>";
 }
 
 echo '<script>
-				$(document).ready(function() {
-					$("select").select2();
-				});
-			</script>';
+$(document).ready(function() {
+	$("select").select2();
+});
+</script>';
 
+// Show the toast message if the message is available.
+if ($toastMessage != "") {
+	echo '<script>
+	toastr.options = {
+		"closeButton": false,
+		"debug": false,
+		"newestOnTop": false,
+		"progressBar": false,
+		"positionClass": "toast-top-full-width",
+		"preventDuplicates": false,
+		"onclick": null,
+		"showDuration": "300",
+		"hideDuration": "1000",
+		"timeOut": "5000",
+		"extendedTimeOut": "1000",
+		"showEasing": "swing",
+		"hideEasing": "linear",
+		"showMethod": "fadeIn",
+		"hideMethod": "fadeOut"
+	}
+	toastr["success"]("' . $toastMessage . '")
+
+	</script>';
+}
 
 echo "</body>";
 echo "</html>";
