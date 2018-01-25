fabbaa6064bf4f426a0fe6de229953addaeb2c1f
Wim De Meester <deepskywim@gmail.com>
2018-01-25 21:43:44 +0100

    Update version number.

--- lib/setup/vars.php
+++ lib/setup/vars.php
@@ -5,8 +5,8 @@ global $inIndex, $copyright, $versionInfo, $copyrightInfo;
 if ((! isset ( $inIndex )) || (! $inIndex))
 	include "../../redirect.php";
 
-$copyright = "&copy;2004&nbsp;-&nbsp;2017";
-$versionInfo = "5.0.53";
+$copyright = "&copy;2004&nbsp;-&nbsp;2018";
+$versionInfo = "2018.1";
 $copyrightInfo = $copyright . ",&nbsp;DeepskyLog developers";
 
 define ( "AtlasOverviewZoom", 17 );
