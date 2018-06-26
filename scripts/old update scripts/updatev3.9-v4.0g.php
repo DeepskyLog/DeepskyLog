5e1dcdc4cd367a2bf530b08921cc380e30a1bd45
Wim De Meester <deepskywim@gmail.com>
2018-06-26 15:27:23 +0200

    Remove old update scripts.

--- scripts/old update scripts/updatev3.9-v4.0g.php
+++
@@ -1,8 +0,0 @@
-<?php
- require_once "../lib/setup/databaseInfo.php";
- require_once "../lib/database.php";
- print "Database update will add the 'locationactive' field on the locations table.\n";
- $sql = "ALTER TABLE locations ADD COLUMN locationactive BOOLEAN NOT NULL DEFAULT true AFTER observer;";
- $run = mysql_query($sql) or die(mysql_error());
- print "Database update successful.\n";
-?>
--- scripts/old update scripts/updatev3.9-v4.0j.php
+++
@@ -1,8 +0,0 @@
-<?php
- require_once "../lib/setup/databaseInfo.php";
- require_once "../lib/database.php";
- print "Database update will add the 'lensactive' field on the lenses table.\n";
- $sql = "ALTER TABLE lenses ADD COLUMN lensactive BOOLEAN NOT NULL DEFAULT true AFTER observer;";
- $run = mysql_query($sql) or die(mysql_error());
- print "Database update successful.\n";
-?>
