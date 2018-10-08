d0c163b068ecdc3af8d2f8e56aa6a53a9fb897c0
Wim De Meester <deepskywim@gmail.com>
2018-10-08 20:49:18 +0200

    Code cleanup of change_instrument and new_instrument.

--- common/content/change_instrument.php
+++ common/content/change_instrument.php
@@ -1,77 +1,139 @@
 <?php
-// change_instrument.php
-// allows the instrument owner or an administrator to change an instrument
-// or another user to view the instrument details
-if ((! isset ( $inIndex )) || (! $inIndex))
-	include "../../redirect.php";
-elseif (! ($instrumentid = $objUtil->checkGetKey ( 'instrument' )))
-	throw new Exception(_("You wanted to change an instrument, but none is specified. Please contact the developers with this message."));
-elseif (! ($objInstrument->getInstrumentPropertyFromId ( $instrumentid, 'name' )))
-	throw new Exception ( "Instrument not found in change_instrument.php, please contact the developers with this message:" . $eyepieceid );
-else
-	change_instrument ();
-function change_instrument() {
-	global $baseURL, $instrumentid, $loggedUser, $objInstrument, $objPresentations, $objUtil;
-	$disabled = " disabled=\"disabled\"";
-	if (($loggedUser) && ($objUtil->checkAdminOrUserID ( $objInstrument->getInstrumentPropertyFromId ( $instrumentid, 'observer', '' ) )))
-		$disabled = "";
-	$content = ($disabled ? "" : "<input type=\"submit\" class=\"btn btn-primary pull-right\" name=\"change\" value=\"" . _("Change instrument") . "\" />&nbsp;");
-	$name = $objInstrument->getInstrumentPropertyFromId ( $instrumentid, 'name' );
-	echo "<div id=\"main\">";
-	echo "<form role=\"form\" action=\"" . $baseURL . "index.php\" method=\"post\"><div>";
-	echo "<input type=\"hidden\" name=\"indexAction\" value=\"validate_instrument\" />";
-	echo "<input type=\"hidden\" name=\"id\" value=\"" . $instrumentid . "\" />";
-	echo "<h4>" . (($name == "Naked eye") ? _("Naked Eye") : $name) . "</h4>";
-	echo "<hr />";
-	echo $content;
-	
-	echo "<div class=\"form-group\">
- 	       <label for=\"filtername\">" . _("Instrument name") . "</label>";
-	echo "<input value=\"" . $name . "\" type=\"text\" required class=\"form-control\" maxlength=\"64\" name=\"instrumentname\" size=\"30\" " . $disabled . " />"; 
-	echo "</div>";
-	
-	$content = "<input value=\"" . round ( $objInstrument->getInstrumentPropertyFromId ( $instrumentid, 'diameter' ), 0 ) . "\" type=\"number\" min=\"0.01\" step=\"0.01\" class=\"form-control\" required maxlength=\"64\" name=\"diameter\" size=\"10\" " . $disabled . " />";
-	$content .= "<select name=\"diameterunits\" class=\"form-control\"" . $disabled . " >";
-	$content .= "<option>inch</option>";
-	$content .= "<option selected=\"selected\">mm</option>";
-	$content .= "</select>";
+/** 
+ * Allows the instrument owner or an administrator to change an instrument
+ * or another user to view the instrument details
+ * 
+ * PHP Version 7
+ * 
+ * @category Utilities/Common
+ * @package  DeepskyLog
+ * @author   DeepskyLog Developers <developers@deepskylog.be>
+ * @license  GPL2 <https://opensource.org/licenses/gpl-2.0.php>
+ * @link     https://www.deepskylog.org
+ */
+if ((!isset($inIndex)) || (!$inIndex)) {
+    include "../../redirect.php";
+} elseif (!($instrumentid = $objUtil->checkGetKey('instrument'))) {
+    throw new Exception(_("You wanted to change an instrument, but none is specified. Please contact the developers with this message."));
+} elseif (!($objInstrument->getInstrumentPropertyFromId($instrumentid, 'name'))) {
+    throw new Exception(_("Instrument not found in change_instrument.php, please contact the developers with this message:" . $eyepieceid));
+} else {
+    changeInstrument();
+}
 
-	echo "<div class=\"form-group\">
- 	       <label for=\"filtername\">" . _("Diameter") . "</label>";
-	echo "<div class=\"form-inline\">";
-	echo $content;
-	echo "</div>";
-	echo "</div>";
-	
-	
-	echo "<div class=\"form-group\">
- 	       <label for=\"filtername\">" . _("Type") . "</label>";
-	echo "<div class=\"form-inline\">";
-	echo $objInstrument->getInstrumentEchoListType ( $objInstrument->getInstrumentPropertyFromId ( $instrumentid, 'type' ), $disabled );
-	echo "</div></div>";
-	
-	$content = "<input value=\"" . (($fl = round ( $objInstrument->getInstrumentPropertyFromId ( $instrumentid, 'fd' ) * $objInstrument->getInstrumentPropertyFromId ( $instrumentid, 'diameter' ), 0 )) ? $fl : "") . "\" type=\"number\" min=\"0.01\" step=\"0.01\" class=\"form-control\" maxlength=\"64\" name=\"focallength\" size=\"10\" " . $disabled . " />";
-	$content .= "<select class=\"form-control\" name=\"focallengthunits\" " . $disabled . " >";
-	$content .= "<option>inch</option>";
-	$content .= "<option selected=\"selected\">mm</option>";
-	$content .= "</select>";
-	$content .= ' ' . _("or F/D") . ' ';
-	$content .= "<input type=\"number\" min=\"0.01\" step=\"0.01\" class=\"form-control\" maxlength=\"64\" name=\"fd\" size=\"10\"  " . $disabled . " />";
+/** 
+ * Change an instrument for the logged in user or administrator
+ * or show the instrument for other users.
+ * 
+ * @return None
+ */
+function changeInstrument() 
+{
+    global $baseURL, $instrumentid, $loggedUser, $objInstrument, $objPresentations; 
+    global $objUtil;
+    $disabled = " disabled=\"disabled\"";
+    if (($loggedUser) 
+        && ($objUtil->checkAdminOrUserID(
+            $objInstrument->getInstrumentPropertyFromId(
+                $instrumentid, 'observer', ''
+            )
+        ))
+    ) {
+        $disabled = "";
+    }
+    $content = ($disabled 
+        ? "" : "<input type=\"submit\" class=\"btn btn-primary pull-right\" " 
+        . "name=\"change\" value=\"" . _("Change instrument") 
+        . "\" />&nbsp;");
+    $name = $objInstrument->getInstrumentPropertyFromId($instrumentid, 'name');
+    echo "<div id=\"main\">";
+    echo "<form role=\"form\" action=\"" . $baseURL 
+        . "index.php\" method=\"post\"><div>";
+    echo "<input type=\"hidden\" name=\"indexAction\" " 
+        . "value=\"validate_instrument\" />";
+    echo "<input type=\"hidden\" name=\"id\" value=\"" . $instrumentid . "\" />";
+    echo "<h4>" . (($name == "Naked eye") ? _("Naked Eye") : $name) . "</h4>";
+    echo "<hr />";
+    echo $content;
+    
+    echo "<div class=\"form-group\">
+            <label for=\"filtername\">" . _("Instrument name") . "</label>";
+    echo "<input value=\"" . $name 
+        . "\" type=\"text\" required class=\"form-control\" maxlength=\"64\" " 
+        . "name=\"instrumentname\" size=\"30\" " . $disabled . " />"; 
+    echo "</div>";
+    
+    $content = "<input value=\"" . round(
+        $objInstrument->getInstrumentPropertyFromId(
+            $instrumentid, 'diameter'
+        ), 0
+    ) . "\" type=\"number\" min=\"0.01\" step=\"0.01\" " 
+        . "class=\"form-control\" required maxlength=\"64\" name=\"diameter\" " 
+        . "size=\"10\" " . $disabled . " />";
+    $content .= "<select name=\"diameterunits\" class=\"form-control\"" 
+        . $disabled . " >";
+    $content .= "<option>inch</option>";
+    $content .= "<option selected=\"selected\">mm</option>";
+    $content .= "</select>";
 
-	echo "<div class=\"form-group\">
- 	       <label for=\"filtername\">" . _("Focal length (in mm)") . "</label>";
-	echo "<div class=\"form-inline\">";
-	echo $content;
-	echo "</div></div>";
-	
-	echo "<div class=\"form-group\">
- 	       <label for=\"filtername\">" . _("Fixed magnification") . "</label>";
-	echo "<div class=\"form-inline\">";
-	echo "<input value=\"" . (($fm = $objInstrument->getInstrumentPropertyFromId ( $instrumentid, 'fixedMagnification' )) ? $fm : "") . "\" type=\"number\" min=\"0.1\" step=\"0.1\" class=\"form-control\" maxlength=\"10\" name=\"fixedMagnification\" size=\"5\" " . $disabled . " />";
-	echo "</div></div>";
-	
-	echo "<hr />";
-	echo "</div></form>";
-	echo "</div>";
+    echo "<div class=\"form-group\">
+            <label for=\"filtername\">" . _("Diameter") . "</label>";
+    echo "<div class=\"form-inline\">";
+    echo $content;
+    echo "</div>";
+    echo "</div>";
+    
+    
+    echo "<div class=\"form-group\">
+            <label for=\"filtername\">" . _("Type") . "</label>";
+    echo "<div class=\"form-inline\">";
+    echo $objInstrument->getInstrumentEchoListType(
+        $objInstrument->getInstrumentPropertyFromId(
+            $instrumentid, 'type'
+        ), $disabled
+    );
+    echo "</div></div>";
+    
+    $content = "<input value=\"" 
+        . (($fl = round(
+            $objInstrument->getInstrumentPropertyFromId($instrumentid, 'fd') 
+            * $objInstrument->getInstrumentPropertyFromId(
+                $instrumentid, 'diameter'
+            ), 0
+        )) ? $fl : "") 
+        . "\" type=\"number\" min=\"0.01\" step=\"0.01\" class=\"form-control\" " 
+        . "maxlength=\"64\" name=\"focallength\" size=\"10\" " . $disabled . " />";
+    $content .= "<select class=\"form-control\" name=\"focallengthunits\" " 
+        . $disabled . " >";
+    $content .= "<option>inch</option>";
+    $content .= "<option selected=\"selected\">mm</option>";
+    $content .= "</select>";
+    $content .= ' ' . _("or F/D") . ' ';
+    $content .= "<input type=\"number\" min=\"0.01\" step=\"0.01\" " 
+        . "class=\"form-control\" maxlength=\"64\" name=\"fd\" size=\"10\"  " 
+        . $disabled . " />";
+
+    echo "<div class=\"form-group\">
+            <label for=\"filtername\">" . _("Focal length (in mm)") . "</label>";
+    echo "<div class=\"form-inline\">";
+    echo $content;
+    echo "</div></div>";
+    
+    echo "<div class=\"form-group\">
+            <label for=\"filtername\">" . _("Fixed magnification") . "</label>";
+    echo "<div class=\"form-inline\">";
+    echo "<input value=\"" 
+        . (($fm = $objInstrument->getInstrumentPropertyFromId(
+            $instrumentid, 'fixedMagnification'
+        )) 
+        ? $fm : "") 
+        . "\" type=\"number\" min=\"0.1\" step=\"0.1\" " 
+        . "class=\"form-control\" maxlength=\"10\" " 
+        . "name=\"fixedMagnification\" size=\"5\" " . $disabled . " />";
+    echo "</div></div>";
+    
+    echo "<hr />";
+    echo "</div></form>";
+    echo "</div>";
 }
 ?>
--- common/content/new_instrument.php
+++ common/content/new_instrument.php
@@ -1,79 +1,162 @@
 <?php
-// new_instrument.php
-// allows the user to add a new instrument
-if ((! isset ( $inIndex )) || (! $inIndex))
-	include "../../redirect.php";
-elseif (! $loggedUser)
-	throw new Exception(_("You need to be logged in to change your locations or equipment."));
-else
-	new_instrument ();
-function new_instrument() {
-	global $baseURL, $loggedUserName, $objInstrument, $objPresentations, $objUtil;
-	echo "<div id=\"main\">";
-	$insts = $objInstrument->getSortedInstruments ( 'name', "", true );
-	echo "<form role=\"form\" action=\"" . $baseURL . "index.php\" method=\"post\"><div>";
-	echo "<input type=\"hidden\" name=\"indexAction\" value=\"validate_instrument\" />";
-	$content1b = "<select class=\"form-control\" onchange=\"location = this.options[this.selectedIndex].value;\" name=\"catalog\">";
-	$content1b .= "<option selected=\"selected\" value=\"" . $baseURL . "index.php?indexAction=add_instrument\"> &nbsp; </option>";
-	while ( list ( $key, $value ) = each ( $insts ) )
-		$content1b .= "<option value=\"" . $baseURL . "index.php?indexAction=add_instrument&amp;instrumentid=" . urlencode ( $value ) . "\" " . (($value == $objUtil->checkRequestKey ( 'instrumentid' )) ? " selected=\"selected\" " : '') . ">" . $objInstrument->getInstrumentPropertyFromId ( $value, 'name' ) . "</option>";
-	$content1b .= "</select>";
+/** 
+ * Add a new instrument for the logged in user.
+ * 
+ * PHP Version 7
+ * 
+ * @category Utilities/Common
+ * @package  DeepskyLog
+ * @author   DeepskyLog Developers <developers@deepskylog.be>
+ * @license  GPL2 <https://opensource.org/licenses/gpl-2.0.php>
+ * @link     https://www.deepskylog.org
+ */
+if ((!isset($inIndex)) || (!$inIndex)) {
+    include "../../redirect.php";
+} elseif (!$loggedUser) {
+    throw new Exception(
+        _("You need to be logged in to change your locations or equipment.")
+    );
+} else {
+    newInstrument();
+}
 
-	echo "<h4>" . _("Add new instrument") . "</h4>";
-	echo "<hr />";
-	echo "<input type=\"submit\" class=\"btn btn-success pull-right tour2\" name=\"add\" value=\"" . _("Add instrument") . "\" />&nbsp;";
+/** 
+ * Add a new instrument for the logged in user.
+ * 
+ * @return None
+ */
+function newInstrument() 
+{
+    global $baseURL, $loggedUserName, $objInstrument, $objPresentations, $objUtil;
+    echo "<div id=\"main\">";
+    $insts = $objInstrument->getSortedInstruments('name', "", true);
+    echo "<form role=\"form\" action=\"" . $baseURL 
+        . "index.php\" method=\"post\"><div>";
+    echo "<input type=\"hidden\" name=\"indexAction\"" 
+        . " value=\"validate_instrument\" />";
+    $content1b = "<select class=\"form-control\" " 
+        . "onchange=\"location = this.options[this.selectedIndex].value;\" " 
+        . "name=\"catalog\">";
+    $content1b .= "<option selected=\"selected\" value=\"" . $baseURL 
+        . "index.php?indexAction=add_instrument\"> &nbsp; </option>";
+    foreach ($insts as $key=>$value) {
+        $content1b .= "<option value=\"" . $baseURL 
+            . "index.php?indexAction=add_instrument&amp;instrumentid=" 
+            . urlencode($value) . "\" " 
+            . (($value == $objUtil->checkRequestKey('instrumentid')) 
+            ? " selected=\"selected\" " : '') 
+            . ">" . $objInstrument->getInstrumentPropertyFromId($value, 'name') 
+            . "</option>";
+    }
+    $content1b .= "</select>";
 
-	echo "<div class=\"form-group\">
-	       <label for=\"catalog\">" . _("Select an existing instrument") . "</label>";
-	echo "<div class=\"form-inline\">";
-	echo $content1b;
-	echo "</div></div>";
+    echo "<h4>" . _("Add new instrument") . "</h4>";
+    echo "<hr />";
+    echo "<input type=\"submit\" class=\"btn btn-success pull-right tour2\" " 
+        . "name=\"add\" value=\"" . _("Add instrument") . "\" />&nbsp;";
 
-	echo _("or add instrument manually");
-	echo "<br /><br />";
+    echo "<div class=\"form-group\">
+           <label for=\"catalog\">" . _("Select an existing instrument") 
+        . "</label>";
+    echo "<div class=\"form-inline\">";
+    echo $content1b;
+    echo "</div></div>";
 
-	$type = $objUtil->checkRequestKey ( 'type' );
-	if ($instrumentid = $objUtil->checkRequestKey ( 'instrumentid', 0 ))
-		$type = $objInstrument->getInstrumentPropertyFromId ( $instrumentid, 'type' );
+    echo _("or add instrument manually");
+    echo "<br /><br />";
 
-	echo "<div class=\"form-group\">
-	       <label for=\"catalog\">" . _("Instrument name") . "</label>";
-	echo "<input type=\"text\" required class=\"form-control\" maxlength=\"64\" name=\"instrumentname\" size=\"30\"  value=\"" . stripslashes ( $objUtil->checkRequestKey ( 'instrumentname' ) ) . stripslashes ( $objInstrument->getInstrumentPropertyFromId ( $objUtil->checkRequestKey ( 'instrumentid' ), 'name' ) ) . "\" />";
-	echo "</div>";
-	echo "</div>";
+    $type = $objUtil->checkRequestKey('type');
+    if ($instrumentid = $objUtil->checkRequestKey('instrumentid', 0)) {
+        $type = $objInstrument->getInstrumentPropertyFromId($instrumentid, 'type');
+    }
 
-	echo "<div class=\"form-group\">
-	       <label for=\"catalog\">" . _("Diameter") . "</label>";
-	echo "<div class=\"form-inline\">";
-	echo "<input type=\"number\" min=\"0.01\" step=\"0.01\" required class=\"form-control\" maxlength=\"64\" name=\"diameter\" size=\"10\" value=\"" . stripslashes ( $objUtil->checkRequestKey ( 'diameter' ) ) . stripslashes ( $objInstrument->getInstrumentPropertyFromId ( $objUtil->checkRequestKey ( 'instrumentid' ), 'diameter' ) ) . "\" />" . "<select name=\"diameterunits\" class=\"form-control\"> <option>inch</option> <option selected=\"selected\">mm</option> </select>";
-	echo "</div>";
-	echo "</div>";
+    echo "<div class=\"form-group\">
+           <label for=\"catalog\">" . _("Instrument name") . "</label>";
+    echo "<input type=\"text\" required class=\"form-control\" maxlength=\"64\" " 
+        . "name=\"instrumentname\" size=\"30\"  value=\"" 
+        . stripslashes($objUtil->checkRequestKey('instrumentname')) 
+        . stripslashes(
+            $objInstrument->getInstrumentPropertyFromId(
+                $objUtil->checkRequestKey('instrumentid'), 'name'
+            )
+        ) . "\" />";
+    echo "</div>";
+    echo "</div>";
 
-	echo "<div class=\"form-group\">
-	       <label for=\"catalog\">" . _("Type") . "</label>";
-	echo "<div class=\"form-inline\">";
-	echo $objInstrument->getInstrumentEchoListType ( $type );
-	echo "</div>";
-	echo "</div>";
+    echo "<div class=\"form-group\">
+           <label for=\"catalog\">" . _("Diameter") . "</label>";
+    echo "<div class=\"form-inline\">";
+    echo "<input type=\"number\" min=\"0.01\" step=\"0.01\" required " 
+        . "class=\"form-control\" maxlength=\"64\" name=\"diameter\" " 
+        . "size=\"10\" value=\"" 
+        . stripslashes($objUtil->checkRequestKey('diameter')) 
+        . stripslashes(
+            $objInstrument->getInstrumentPropertyFromId(
+                $objUtil->checkRequestKey('instrumentid'), 'diameter'
+            )
+        ) . "\" />" 
+        . "<select name=\"diameterunits\" class=\"form-control\"> " 
+        . "<option>inch</option> <option selected=\"selected\">mm</option> " 
+        . "</select>";
+    echo "</div>";
+    echo "</div>";
 
-	echo "<div class=\"form-group\">
-	       <label for=\"catalog\">" . _("F/D") . "</label>";
-	echo "<div class=\"form-inline\">";
-	echo "<input type=\"number\" min=\"0.0\" step=\"0.0\" class=\"form-control\" maxlength=\"64\" name=\"focallength\" size=\"10\"  value=\"" . stripslashes ( $objUtil->checkRequestKey ( 'focallength' ) ) . stripslashes ( $objInstrument->getInstrumentPropertyFromId ( $objUtil->checkRequestKey ( 'instrumentid' ), 'diameter' ) * $objInstrument->getInstrumentPropertyFromId ( $objUtil->checkRequestKey ( 'instrumentid' ), 'fd' ) ) . "\" />" . "<select class=\"form-control\" name=\"focallengthunits\"> <option>inch</option> <option selected=\"selected\">mm</option> </select>" . "&nbsp;<span>" . _("or F/D") . "</span>&nbsp;" . "<input type=\"number\" min=\"0.0\" step=\"0.01\" class=\"form-control\" maxlength=\"64\" name=\"fd\" size=\"10\" value=\"" . stripslashes ( $objUtil->checkRequestKey ( 'fd' ) ) . stripslashes ( $objInstrument->getInstrumentPropertyFromId ( $objUtil->checkRequestKey ( 'instrumentid' ), 'fd' ) ) . "\" />";
-	echo "</div>";
-	echo "</div>";
+    echo "<div class=\"form-group\">
+           <label for=\"catalog\">" . _("Type") . "</label>";
+    echo "<div class=\"form-inline\">";
+    echo $objInstrument->getInstrumentEchoListType($type);
+    echo "</div>";
+    echo "</div>";
 
-	echo "<div class=\"form-group\">
-	       <label for=\"catalog\">" . _("Fixed magnification") . "</label>";
-	echo "<div class=\"form-inline\">";
-	echo "<input type=\"number\" min=\"0.0\" step=\"0.1\" class=\"form-control\" maxlength=\"5\" name=\"fixedMagnification\" size=\"5\" value=\"" . ($objUtil->checkRequestKey ( 'fixedMagnification' )) . stripslashes ( $objInstrument->getInstrumentPropertyFromId ( $objUtil->checkRequestKey ( 'instrumentid' ), 'fixedMagnification' ) ) . "\" />";
-	echo "</div>";
-	echo "<span class=\"help-block\">" . _("Only for binoculars, finder scopes, ...") . "</span>";
-	echo "</div>";
+    echo "<div class=\"form-group\">
+           <label for=\"catalog\">" . _("F/D") . "</label>";
+    echo "<div class=\"form-inline\">";
+    echo "<input type=\"number\" min=\"0.0\" step=\"0.0\" class=\"form-control\"" 
+        . " maxlength=\"64\" name=\"focallength\" size=\"10\"  value=\"" 
+        . stripslashes($objUtil->checkRequestKey('focallength')) 
+        . stripslashes(
+            $objInstrument->getInstrumentPropertyFromId(
+                $objUtil->checkRequestKey('instrumentid'), 'diameter'
+            ) * $objInstrument->getInstrumentPropertyFromId(
+                $objUtil->checkRequestKey('instrumentid'), 'fd'
+            )
+        ) . "\" />" 
+        . "<select class=\"form-control\" name=\"focallengthunits\">" 
+        . "<option>inch</option> <option selected=\"selected\">mm</option>" 
+        . "</select>" 
+        . "&nbsp;<span>" . _("or F/D") . "</span>&nbsp;" 
+        . "<input type=\"number\" min=\"0.0\" step=\"0.01\" " 
+        . "class=\"form-control\" maxlength=\"64\" name=\"fd\" " 
+        . "size=\"10\" value=\"" 
+        . stripslashes($objUtil->checkRequestKey('fd')) 
+        . stripslashes(
+            $objInstrument->getInstrumentPropertyFromId(
+                $objUtil->checkRequestKey('instrumentid'), 'fd'
+            )
+        ) . "\" />";
+    echo "</div>";
+    echo "</div>";
 
-	echo "<hr />";
-	echo "<input type=\"submit\" class=\"btn btn-success\" name=\"add\" value=\"" . _("Add instrument") . "\" />&nbsp;";
-	echo "</div></form>";
-	echo "</div>";
+    echo "<div class=\"form-group\">
+           <label for=\"catalog\">" . _("Fixed magnification") . "</label>";
+    echo "<div class=\"form-inline\">";
+    echo "<input type=\"number\" min=\"0.0\" step=\"0.1\" class=\"form-control\"" 
+        . " maxlength=\"5\" name=\"fixedMagnification\" size=\"5\" value=\"" 
+        . ($objUtil->checkRequestKey('fixedMagnification')) 
+        . stripslashes(
+            $objInstrument->getInstrumentPropertyFromId(
+                $objUtil->checkRequestKey('instrumentid'), 'fixedMagnification'
+            )
+        ) . "\" />";
+    echo "</div>";
+    echo "<span class=\"help-block\">" 
+        . _("Only for binoculars, finder scopes, ...") . "</span>";
+    echo "</div>";
+
+    echo "<hr />";
+    echo "<input type=\"submit\" class=\"btn btn-success\" name=\"add\" value=\"" 
+        . _("Add instrument") . "\" />&nbsp;";
+    echo "</div></form>";
+    echo "</div>";
 }
 ?>
