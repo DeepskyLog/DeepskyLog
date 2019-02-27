e69ea1d495b5dbdadafddecb53cecc37ec85c163
Wim De Meester <deepskywim@gmail.com>
2019-02-27 20:50:56 +0100

    Show errors on pages.

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -23,13 +23,13 @@
                 @include('layout.sidebar')
 
                 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-4">
+                    @include('layout.errors')
+
                     <br />
                     @yield('content')
                 </main>
             </div>
         </div>
-
-        @include('layout.errors')
 
         <br />
         @include('layout.footer')
