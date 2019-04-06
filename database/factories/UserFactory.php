567cef575b36690b316ce0fa97b739c4434b78e0
Wim De Meester <deepskywim@gmail.com>
2019-04-06 16:35:34 +0100

    Add seeders

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,6 +19,8 @@ $factory->define(App\User::class, function (Faker $faker) {
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
+        'country' => 'be',
+        'copyright' => 'Attribution-NoDerivs CC BY-ND',
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => Str::random(10),
     ];
