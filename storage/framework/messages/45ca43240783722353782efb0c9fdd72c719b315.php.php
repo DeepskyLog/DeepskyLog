668166d86d2320697015aac5e8a143ee2fc27fc7
Wim De Meester <deepskywim@gmail.com>
2019-05-29 11:07:20 +0200

    Remove files in storage/framework/messages

--- storage/framework/messages/45ca43240783722353782efb0c9fdd72c719b315.php.php
+++
@@ -1,2 +0,0 @@
-<?php echo e($slot); ?>: <?php echo e($url); ?>
-
--- storage/framework/messages/7434371ec9c182a37226ee7514fc5e4ea715e7b1.php.php
+++
@@ -1,8 +0,0 @@
-
-<?php if(sizeof(laraflash()->toArray()) > 0): ?>
-    <br />
-    <div class="container">
-        <?php echo laraflash()->render(); ?>
-
-    </div>
-<?php endif; ?>
