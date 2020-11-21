bc748edcd8acf854bc979f25fe48545744d1dfa3
Wim De Meester <deepskywim@gmail.com>
2020-11-21 21:18:35 +0100

    Added DeepskyLog mailbox to the user menu

--- resources/views/layout/header/user.blade.php
+++ resources/views/layout/header/user.blade.php
@@ -8,6 +8,8 @@
         <a class="dropdown-item" href="/users/{{ Auth::user()->slug }}">{{ _i('Details') }}</a>
         <a class="dropdown-item" href="/users/{{ Auth::user()->slug }}/settings">{{ _i('Settings') }}</a>
         <a class="dropdown-item disabled" href="#">───────────────────</a>
+        <a class="dropdown-item" href="/messages">{{ _i('DeepskyLog Mailbox') }}</a>
+        <a class="dropdown-item disabled" href="#">───────────────────</a>
         <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
             document.getElementById('logout-form').submit();">
             {{ _i('Log out') }}
