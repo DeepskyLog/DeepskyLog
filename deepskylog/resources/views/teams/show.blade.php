52ad75b7c4337dc49b91b841c4ee3c6d75fc7ae5
Wim De Meester <wim.demeester@kuleuven.be>
2023-01-31 16:23:31 +0100

    Remove the option to delete a group.

--- deepskylog/resources/views/teams/show.blade.php
+++ deepskylog/resources/views/teams/show.blade.php
@@ -9,17 +9,10 @@
         <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8 bg-gray-900">
             @livewire('teams.update-team-name-form', ['team' => $team])
 
-            @if (Auth::user()->isAdministrator())
+            @if (Auth::user()->hasAdministratorPrivileges())
                 @livewire('teams.team-member-manager', ['team' => $team])
             @endif
 
-            @if (Gate::check('delete', $team) && !$team->personal_team)
-                <x-jet-section-border />
-
-                <div class="mt-10 sm:mt-0">
-                    @livewire('teams.delete-team-form', ['team' => $team])
-                </div>
-            @endif
         </div>
     </div>
 </x-app-layout>
