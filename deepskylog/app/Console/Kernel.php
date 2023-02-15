a0f92a44a40bb7f6a57258abed309536ebcaf2ea
Wim De Meester <wim.demeester@kuleuven.be>
2023-02-15 16:40:34 +0100

    Update to laravel 10.

--- deepskylog/app/Console/Kernel.php
+++ deepskylog/app/Console/Kernel.php
@@ -13,9 +13,10 @@ class Kernel extends ConsoleKernel
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
      */
-    protected function schedule(Schedule $schedule)
+    protected function schedule(Schedule $schedule): void
     {
         $schedule->command('auth:clear-resets')->everyFifteenMinutes();
+        $schedule->command('cache:prune-stale-tags')->hourly();
     }
 
     /**
@@ -23,7 +24,7 @@ class Kernel extends ConsoleKernel
      *
      * @return void
      */
-    protected function commands()
+    protected function commands(): void
     {
         $this->load(__DIR__.'/Commands');
 
