77187404963df56786f48d4212934b74e12ee883
Wim De Meester <wim.demeester@kuleuven.be>
2023-02-23 14:06:26 +0100

    Only show the dropdown menu for the teams if the user belongs to more than 1 team.

--- deepskylog/resources/views/navigation-menu.blade.php
+++ deepskylog/resources/views/navigation-menu.blade.php
@@ -20,7 +20,7 @@
 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
                 <!-- Teams Dropdown -->
-                @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
+                @if (Laravel\Jetstream\Jetstream::hasTeamFeatures() && Auth::user()->teams->count() > 1)
                     <div class="ml-3 relative">
                         <x-dropdown align="right" width="60">
                             <x-slot name="trigger">
